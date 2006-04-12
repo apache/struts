ffceeb61acf5bc879aab886b8f9e576e71620283
Rainer Hermanns <hermanns@apache.org>
2006-04-12 17:29:13 +0000

    ANT Unit tests broken o fixed the unit test targets o readded -Dtestcase=<NameOfTestClass> Issue Number: WW-1283
    git-svn-id: https://svn.apache.org/repos/asf/incubator/webwork2@393539 13f79535-47bb-0310-9956-ffa450edef68

--- action/src/test/java/org/apache/struts/action2/sitegraph/SiteGraphTest.java
+++ action/src/test/java/org/apache/struts/action2/sitegraph/SiteGraphTest.java
@@ -22,6 +22,7 @@ import org.apache.struts.action2.StrutsTestCase;
 
 import java.io.File;
 import java.io.StringWriter;
+import java.io.InputStream;
 import java.net.URL;
 
 /**
@@ -30,11 +31,11 @@ import java.net.URL;
  * Time: 4:18:28 PM
  */
 public class SiteGraphTest extends StrutsTestCase {
-    public void testWebFlow() {
+    public void testWebFlow() throws Exception {
         // use the classloader rather than relying on the
         // working directory being an assumed value when
         // running the test:  so let's get this class's parent dir 
-        URL url = this.getClass().getClassLoader().getResource(this.getClass().getName().replace(".", "/") + ".class");
+        URL url = this.getClass().getClassLoader().getResource(this.getClass().getName().replace('.', '/') + ".class");
         File file = new File(url.toString().substring(5));
         String dir = file.getParent();
         SiteGraph siteGraph = new SiteGraph(dir, dir, dir, "");
@@ -42,7 +43,18 @@ public class SiteGraphTest extends StrutsTestCase {
         siteGraph.setWriter(writer);
         siteGraph.prepare();
 
-        String out = dir + "/out.txt";
-        assertEquals(FileUtils.readFile(new File(out)), writer.toString());
+        URL compare = SiteGraphTest.class.getResource("out.txt");
+        StringBuffer buffer = new StringBuffer(128);
+        InputStream in = compare.openStream();
+        byte[] buf = new byte[4096];
+        int nbytes;
+
+        while ((nbytes = in.read(buf)) > 0) {
+            buffer.append(new String(buf, 0, nbytes));
+        }
+
+        in.close();
+
+        assertEquals(buffer.toString(), writer.toString());
     }
 }

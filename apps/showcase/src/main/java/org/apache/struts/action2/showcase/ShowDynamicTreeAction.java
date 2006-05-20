ea5121498edae4c7b8f60d2d5f0e6e4ef6c2bf1d
Toby Jee <tmjee@apache.org>
2006-05-20 16:59:30 +0000

    - added snippet
    git-svn-id: https://svn.apache.org/repos/asf/struts/action2/trunk@408036 13f79535-47bb-0310-9956-ffa450edef68

--- apps/showcase/src/main/java/org/apache/struts/action2/showcase/ShowDynamicTreeAction.java
+++ apps/showcase/src/main/java/org/apache/struts/action2/showcase/ShowDynamicTreeAction.java
@@ -4,6 +4,7 @@ import org.apache.struts.action2.showcase.ajax.tree.Category;
 
 import com.opensymphony.xwork.ActionSupport;
 
+// START SNIPPET: treeExampleDynamicJavaShow 
 
 public class ShowDynamicTreeAction extends ActionSupport {
 	
@@ -11,4 +12,6 @@ public class ShowDynamicTreeAction extends ActionSupport {
 		return Category.getById(1);
 	}
 }
+
+// END SNIPPET: treeExampleDynamicJavaShow
 

103bdebb8c7ad0550d633bf9811af63fbb561ee3
Toby Jee <tmjee@apache.org>
2006-07-22 10:34:07 +0000

    WW-1389   - Added more tests to test If/Else/ElseIf tag
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@424544 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/views/jsp/IfTagTest.java
+++ core/src/test/java/org/apache/struts2/views/jsp/IfTagTest.java
@@ -149,6 +149,115 @@ public class IfTagTest extends TestCase {
             fail();
         }
     }
+    
+    
+    public void testIfElse1() throws Exception {
+    	IfTag ifTag = new IfTag();
+    	ifTag.setPageContext(pageContext);
+    	ifTag.setTest("true");
+    	
+    	ElseTag elseTag = new ElseTag();
+    	elseTag.setPageContext(pageContext);
+    	
+    	int r1 = ifTag.doStartTag();
+    	ifTag.doEndTag();
+    	int r2 = elseTag.doStartTag();
+    	elseTag.doEndTag();
+    	
+    	assertEquals(TagSupport.EVAL_BODY_INCLUDE, r1);
+    	assertEquals(TagSupport.SKIP_BODY, r2);
+    }
+    
+    public void testIfElse2() throws Exception {
+    	IfTag ifTag = new IfTag();
+    	ifTag.setPageContext(pageContext);
+    	ifTag.setTest("false");
+    	
+    	ElseTag elseTag = new ElseTag();
+    	elseTag.setPageContext(pageContext);
+    	
+    	int r1 = ifTag.doStartTag();
+    	ifTag.doEndTag();
+    	int r2 = elseTag.doStartTag();
+    	elseTag.doEndTag();
+    	
+    	assertEquals(TagSupport.SKIP_BODY, r1);
+    	assertEquals(TagSupport.EVAL_BODY_INCLUDE, r2);
+    }
+    
+    public void testIfElseIf() throws Exception {
+    	IfTag ifTag = new IfTag();
+    	ifTag.setPageContext(pageContext);
+    	ifTag.setTest("false");
+    	
+    	ElseIfTag elseIfTag1 = new ElseIfTag();
+    	elseIfTag1.setPageContext(pageContext);
+    	elseIfTag1.setTest("false");
+    	
+    	ElseIfTag elseIfTag2 = new ElseIfTag();
+    	elseIfTag2.setPageContext(pageContext);
+    	elseIfTag2.setTest("true");
+    	
+    	ElseIfTag elseIfTag3 = new ElseIfTag();
+    	elseIfTag3.setPageContext(pageContext);
+    	elseIfTag3.setTest("true");
+    	
+    	int r1 = ifTag.doStartTag();
+    	ifTag.doEndTag();
+    	int r2 = elseIfTag1.doStartTag();
+    	elseIfTag1.doEndTag();
+    	int r3 = elseIfTag2.doStartTag();
+    	elseIfTag2.doEndTag();
+    	int r4 = elseIfTag3.doStartTag();
+    	elseIfTag3.doEndTag();
+    	
+    	assertEquals(TagSupport.SKIP_BODY, r1);
+    	assertEquals(TagSupport.SKIP_BODY, r2);
+    	assertEquals(TagSupport.EVAL_BODY_INCLUDE, r3);
+    	assertEquals(TagSupport.SKIP_BODY, r4);
+    }
+    
+    public void testIfElseIfElse() throws Exception {
+    	IfTag ifTag = new IfTag();
+    	ifTag.setPageContext(pageContext);
+    	ifTag.setTest("false");
+    	
+    	ElseIfTag elseIfTag1 = new ElseIfTag();
+    	elseIfTag1.setPageContext(pageContext);
+    	elseIfTag1.setTest("false");
+    	
+    	ElseIfTag elseIfTag2 = new ElseIfTag();
+    	elseIfTag2.setPageContext(pageContext);
+    	elseIfTag2.setTest("false");
+    	
+    	ElseIfTag elseIfTag3 = new ElseIfTag();
+    	elseIfTag3.setPageContext(pageContext);
+    	elseIfTag3.setTest("false");
+    	
+    	ElseTag elseTag = new ElseTag();
+    	elseTag.setPageContext(pageContext);
+    	
+    	int r1 = ifTag.doStartTag();
+    	ifTag.doEndTag();
+    	int r2 = elseIfTag1.doStartTag();
+    	elseIfTag1.doEndTag();
+    	int r3 = elseIfTag2.doStartTag();
+    	elseIfTag2.doEndTag();
+    	int r4 = elseIfTag3.doStartTag();
+    	elseIfTag3.doEndTag();
+    	int r5 = elseTag.doStartTag();
+    	elseTag.doEndTag();
+    	
+    	assertEquals(TagSupport.SKIP_BODY, r1);
+    	assertEquals(TagSupport.SKIP_BODY, r2);
+    	assertEquals(TagSupport.SKIP_BODY, r3);
+    	assertEquals(TagSupport.SKIP_BODY, r4);
+    	assertEquals(TagSupport.EVAL_BODY_INCLUDE, r5);
+    }
+    
+    
+    
+    
 
     protected void setUp() throws Exception {
         // create the needed objects

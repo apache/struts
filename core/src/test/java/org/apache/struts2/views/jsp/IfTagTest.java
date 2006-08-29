272d1a07a739d5f14c4531e2f0dca15af322459d
Toby Jee <tmjee@apache.org>
2006-08-29 18:31:13 +0000

    WW-1422  - Else tag body displayed when it shouldn't
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@438147 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/views/jsp/IfTagTest.java
+++ core/src/test/java/org/apache/struts2/views/jsp/IfTagTest.java
@@ -256,6 +256,60 @@ public class IfTagTest extends TestCase {
     }
     
     
+    public void testNestedIfElse1() throws Exception {
+    	IfTag ifTag = new IfTag();
+    	ifTag.setPageContext(pageContext);
+    	ifTag.setTest("true");
+    	
+    	IfTag nestedIfTag = new IfTag();
+    	nestedIfTag.setPageContext(pageContext);
+    	nestedIfTag.setTest("true");
+    	
+    	ElseTag elseTag = new ElseTag();
+    	elseTag.setPageContext(pageContext);
+    	
+    	int r1 = ifTag.doStartTag();
+    	int r2 = nestedIfTag.doStartTag();
+    	int r3 = nestedIfTag.doEndTag();
+    	int r4 = ifTag.doEndTag();
+    	int r5 = elseTag.doStartTag();
+    	int r6 = elseTag.doEndTag();
+    	
+    	assertEquals(TagSupport.EVAL_BODY_INCLUDE, r1);
+    	assertEquals(TagSupport.EVAL_BODY_INCLUDE, r2);
+    	assertEquals(TagSupport.EVAL_PAGE, r3);
+    	assertEquals(TagSupport.EVAL_PAGE, r4);
+    	assertEquals(TagSupport.SKIP_BODY, r5);
+    	assertEquals(TagSupport.EVAL_PAGE, r6);
+    }
+    
+    public void testNestedIfElse2() throws Exception {
+    	IfTag ifTag = new IfTag();
+    	ifTag.setPageContext(pageContext);
+    	ifTag.setTest("true");
+    	
+    	IfTag nestedIfTag = new IfTag();
+    	nestedIfTag.setPageContext(pageContext);
+    	nestedIfTag.setTest("false");
+    	
+    	ElseTag elseTag = new ElseTag();
+    	elseTag.setPageContext(pageContext);
+    	
+    	int r1 = ifTag.doStartTag();
+    	int r2 = nestedIfTag.doStartTag();
+    	int r3 = nestedIfTag.doEndTag();
+    	int r4 = ifTag.doEndTag();
+    	int r5 = elseTag.doStartTag();
+    	int r6 = elseTag.doEndTag();
+    	
+    	assertEquals(TagSupport.EVAL_BODY_INCLUDE, r1);
+    	assertEquals(TagSupport.SKIP_BODY, r2);
+    	assertEquals(TagSupport.EVAL_PAGE, r3);
+    	assertEquals(TagSupport.EVAL_PAGE, r4);
+    	assertEquals(TagSupport.SKIP_BODY, r5);
+    	assertEquals(TagSupport.EVAL_PAGE, r6);
+    }
+    
     
     
 

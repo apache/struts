a7429cc31e2016cc9319080a19c679632ff4dedb
Donald J. Brown <mrdon@apache.org>
2006-11-06 20:04:56 +0000

    Removing unused MockConfiguration import
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@471864 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/config/ClasspathConfigurationProviderTest.java
+++ core/src/test/java/org/apache/struts2/config/ClasspathConfigurationProviderTest.java
@@ -30,7 +30,6 @@ import com.opensymphony.xwork2.config.entities.PackageConfig;
 import com.opensymphony.xwork2.config.entities.ResultConfig;
 import com.opensymphony.xwork2.config.entities.ResultTypeConfig;
 import com.opensymphony.xwork2.config.impl.DefaultConfiguration;
-import com.opensymphony.xwork2.config.impl.MockConfiguration;
 
 import junit.framework.TestCase;
 

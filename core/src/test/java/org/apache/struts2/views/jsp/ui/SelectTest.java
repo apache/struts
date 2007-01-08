39cd58cdaeb4c881f24a5dc2817e18ca42c5a1fa
Donald J. Brown <mrdon@apache.org>
2007-01-08 19:05:40 +0000

    Added test for byte key in select tag WW-1601
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@494155 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/views/jsp/ui/SelectTest.java
+++ core/src/test/java/org/apache/struts2/views/jsp/ui/SelectTest.java
@@ -51,6 +51,48 @@ public class SelectTest extends AbstractUITagTest {
     }
 
     /**
+     * Tests WW-1601: Select tag template does not work properly for Object like Byte.
+     */
+    public void testByte() throws Exception {
+        ByteObject hello = new ByteObject(new Byte((byte)1), "hello");
+        ByteObject foo = new ByteObject(new Byte((byte)2), "foo");
+
+        TestAction testAction = (TestAction) action;
+
+        Collection collection = new ArrayList(2);
+        // expect strings to be returned, we're still dealing with HTTP here!
+        collection.add("1");
+        collection.add("2");
+        testAction.setCollection(collection);
+
+        List list2 = new ArrayList();
+        list2.add(hello);
+        list2.add(foo);
+        testAction.setList2(list2);
+
+        SelectTag tag = new SelectTag();
+        tag.setPageContext(pageContext);
+        tag.setLabel("mylabel");
+        tag.setName("collection");
+        tag.setList("list2");
+        tag.setListKey("byte");
+        tag.setListValue("name");
+        tag.setMultiple("true");
+        tag.setTitle("mytitle");
+        tag.setOnmousedown("alert('onmousedown');");
+        tag.setOnmousemove("alert('onmousemove');");
+        tag.setOnmouseout("alert('onmouseout');");
+        tag.setOnmouseover("alert('onmouseover');");
+        tag.setOnmouseup("alert('onmouseup');");
+
+        tag.doStartTag();
+        tag.doEndTag();
+
+        verify(SelectTag.class.getResource("Select-10.txt"));
+    }
+
+
+    /**
      * Tests WW-455: Select tag template does not work properly for Object like BigDecimal.
      */
     public void testBigDecimal() throws Exception {
@@ -107,6 +149,24 @@ public class SelectTest extends AbstractUITagTest {
 
         public BigDecimal getBigDecimal() {
             return bigDecimal;
+        }
+    }
+
+    public class ByteObject {
+        private String name;
+        private Byte byteValue;
+
+        public ByteObject(Byte byteValue, String name) {
+            this.name = name;
+            this.byteValue = byteValue;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Byte getByte() {
+            return byteValue;
         }
     }
 

ac8b7404f180c932e4d6e228df6c224e86d031c3
Ted Nathan Husted <husted@apache.org>
2007-01-29 03:51:53 +0000

    Update POMs for Struts 2.0.5 SNAPSHOT.
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@500934 13f79535-47bb-0310-9956-ffa450edef68

--- backport/translate.bat
+++ backport/translate.bat
@@ -1,4 +1,3 @@
-java -jar retrotranslator-transformer-1.1.0.jar -srcjar ../lib/struts2-core-2.0.3.jar -destjar struts2-core-j4-2.0.3.jar 
-java -jar retrotranslator-transformer-1.1.0.jar -srcjar ../lib/struts2-api-2.0.3.jar -destjar struts2-api-j4-2.0.3.jar 
-java -jar retrotranslator-transformer-1.1.0.jar -srcjar ../lib/struts2-extras-2.0.3.jar -destjar struts2-extras-j4-2.0.3.jar 
-java -jar retrotranslator-transformer-1.1.0.jar -srcjar ../lib/xwork-2.0.0.jar -destjar xwork-j4-2.0.0-RC1.jar 
+java -jar retrotranslator-transformer-1.2.0.jar -advanced -srcjar ../lib/struts2-core-2.0.4.jar -destjar struts2-core-j4-2.0.4.jar 
+java -jar retrotranslator-transformer-1.2.0.jar -advanced -srcjar ../lib/struts2-api-2.0.4.jar -destjar struts2-api-j4-2.0.4.jar 
+java -jar retrotranslator-transformer-1.2.0.jar -advanced -srcjar ../lib/xwork-2.0.0.jar -destjar xwork-j4-2.0.0.jar 

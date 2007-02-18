0a388b18e43dee88c39f4e15571f175716676c0c
Thomas Schneider <tschneider@apache.org>
2007-02-18 06:32:02 +0000

    WW-1709 fixed nullpointer when scheme is null
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@508879 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/views/util/UrlHelperTest.java
+++ core/src/test/java/org/apache/struts2/views/util/UrlHelperTest.java
@@ -90,7 +90,26 @@ public class UrlHelperTest extends StrutsTestCase {
            expectedUrl, url.toString());
     }
 
+    public void testForceAddNullSchemeHostAndPort() throws Exception {
+        String expectedUrl = "http://localhost/contextPath/path1/path2/myAction.action";
 
+        Mock mockHttpServletRequest = new Mock(HttpServletRequest.class);
+        mockHttpServletRequest.expectAndReturn("getScheme", "http");
+        mockHttpServletRequest.expectAndReturn("getServerName", "localhost");
+        mockHttpServletRequest.expectAndReturn("getContextPath",
+            "/contextPath");
+
+        Mock mockHttpServletResponse = new Mock(HttpServletResponse.class);
+        mockHttpServletResponse.expectAndReturn("encodeURL", expectedUrl,
+            expectedUrl);
+
+        String result = UrlHelper.buildUrl("/path1/path2/myAction.action",
+            (HttpServletRequest) mockHttpServletRequest.proxy(),
+            (HttpServletResponse) mockHttpServletResponse.proxy(), null,
+            null, true, true, true);
+        assertEquals(expectedUrl, result);
+        mockHttpServletRequest.verify();
+    }
 
     public void testBuildWithRootContext() {
         String expectedUrl = "/MyAction.action";

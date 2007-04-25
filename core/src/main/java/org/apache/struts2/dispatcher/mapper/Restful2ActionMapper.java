b9d365a04c8a5a47f79d0f38d014a60b642e09af
Donald J. Brown <mrdon@apache.org>
2007-04-25 14:18:34 +0000

    Minor javadoc fix WW-1834
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/branches/STRUTS_2_0_X@532363 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/struts2/dispatcher/mapper/Restful2ActionMapper.java
+++ core/src/main/java/org/apache/struts2/dispatcher/mapper/Restful2ActionMapper.java
@@ -58,8 +58,8 @@ import org.apache.commons.logging.LogFactory;
  * <p>
  * The following URL's will invoke its methods:
  * </p>
- * <ul> 
- *  <li><code>GET:    /movie                => method="index"</code></li>
+ * <ul>
+ *  <li><code>GET:    /movie/               => method="index"</code></li>
  *  <li><code>GET:    /movie/Thrillers      => method="view", id="Thrillers"</code></li>
  *  <li><code>GET:    /movie/Thrillers!edit => method="edit", id="Thrillers"</code></li>
  *  <li><code>GET:    /movie/new            => method="editNew"</code></li>

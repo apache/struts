9a278838c2837b7df78c6c0f14103429760b1339
Donald J. Brown <mrdon@apache.org>
2007-05-20 03:06:34 +0000

    Improving opt groups to use toString on key, adding tests WW-1672
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/branches/STRUTS_2_0_X@539819 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/dispatcher/ServletRedirectResultTest.java
+++ core/src/test/java/org/apache/struts2/dispatcher/ServletRedirectResultTest.java
@@ -97,6 +97,20 @@ public class ServletRedirectResultTest extends StrutsTestCase implements StrutsS
         requestMock.verify();
         responseMock.verify();
     }
+    
+    public void testMultipleParametersRedirect() throws Exception {
+        view.setLocation("foo.jsp?foo=bar&amp;baz=jim");
+        requestMock.expectAndReturn("getParameterMap", new HashMap());
+        requestMock.expectAndReturn("getServletPath", "/namespace/some.action");
+        requestMock.expectAndReturn("getAttribute", C.ANY_ARGS, null);
+        responseMock.expectAndReturn("encodeRedirectURL", "/context/namespace/foo.jsp?foo=bar&amp;baz=jim", "/context/namespace/foo.jsp?foo=bar&baz=jim");
+        responseMock.expect("sendRedirect", C.args(C.eq("/context/namespace/foo.jsp?foo=bar&baz=jim")));
+
+        view.execute(ai);
+
+        requestMock.verify();
+        responseMock.verify();
+    }
 
     protected void setUp() throws Exception {
         super.setUp();

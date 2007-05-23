d176c997bffd66e3a1470f4c8a8051d38fc745d9
Antonio Petrelli <apetrelli@apache.org>
2007-05-23 15:16:35 +0000

    WW-1941 Merge from trunk to STRUTS_2_0_X branch. Updated Struts 2 - Tiles 2 plugin to use Tiles 2.0.3 in trunk. Updated Struts 2 showcase to the changes of Struts 2 - Tiles 2 plugin.
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/branches/STRUTS_2_0_X@540974 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/tiles/src/main/java/org/apache/struts2/views/tiles/TilesResult.java
+++ plugins/tiles/src/main/java/org/apache/struts2/views/tiles/TilesResult.java
@@ -101,6 +101,6 @@ public class TilesResult extends ServletDispatcherResult {
         HttpServletRequest request = ServletActionContext.getRequest();
         HttpServletResponse response = ServletActionContext.getResponse();
 
-        container.render(request, response, location);
+        container.render(location, request, response);
     }
 }

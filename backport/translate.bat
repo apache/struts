8373599f014c963cc87a8d68c8b6a22d1287e99a
Rainer Hermanns <hermanns@apache.org>
2007-06-06 14:45:50 +0000

    Removing SNAPSHOT for Struts 2.0.8 release o updated all pom.xml o updated backport/translate.bat o updated dependency on struts-annotations to 1.0.2 release
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/branches/STRUTS_2_0_X@544857 13f79535-47bb-0310-9956-ffa450edef68

--- backport/translate.bat
+++ backport/translate.bat
@@ -1,2 +1,2 @@
-java -jar retrotranslator-transformer-1.2.0.jar -advanced -srcjar ../lib/struts2-core-2.0.6.jar -destjar struts2-core-j4-2.0.6.jar 
-java -jar retrotranslator-transformer-1.2.0.jar -advanced -srcjar ../lib/xwork-2.0.1.jar -destjar xwork-j4-2.0.1.jar 
+java -jar retrotranslator-transformer-1.2.0.jar -advanced -srcjar ../lib/struts2-core-2.0.8.jar -destjar struts2-core-j4-2.0.8.jar
+java -jar retrotranslator-transformer-1.2.0.jar -advanced -srcjar ../lib/xwork-2.0.3.jar -destjar xwork-j4-2.0.3.jar 

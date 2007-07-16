604388159c904de5bb8d38578d2da66996e79003
Donald J. Brown <mrdon@apache.org>
2007-07-16 13:35:09 +0000

    Adding tests to ensure recursive value attribute processing has been disabled WW-2030
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@556624 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/views/jsp/ui/TextfieldTest.java
+++ core/src/test/java/org/apache/struts2/views/jsp/ui/TextfieldTest.java
@@ -148,4 +148,37 @@ public class TextfieldTest extends AbstractUITagTest {
 
         verify(TextFieldTag.class.getResource("Textfield-1.txt"));
     }
+    
+    public void testSimple_recursionTest() throws Exception {
+        TestAction testAction = (TestAction) action;
+        testAction.setFoo("%{1+1}");
+
+        TextFieldTag tag = new TextFieldTag();
+        tag.setPageContext(pageContext);
+        tag.setLabel("mylabel");
+        tag.setName("myname");
+        tag.setValue("%{foo}");
+        tag.setSize("10");
+
+        tag.doStartTag();
+        tag.doEndTag();
+
+        verify(TextFieldTag.class.getResource("Textfield-5.txt"));
+    }
+    
+    public void testSimple_recursionTestNoValue() throws Exception {
+        TestAction testAction = (TestAction) action;
+        testAction.setFoo("%{1+1}");
+
+        TextFieldTag tag = new TextFieldTag();
+        tag.setPageContext(pageContext);
+        tag.setLabel("mylabel");
+        tag.setName("foo");
+        tag.setSize("10");
+
+        tag.doStartTag();
+        tag.doEndTag();
+
+        verify(TextFieldTag.class.getResource("Textfield-6.txt"));
+    }
 }

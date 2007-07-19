329bb930f0e3ed2d7541050baed92be232dd2db1
Rainer Hermanns <hermanns@apache.org>
2007-07-19 17:31:26 +0000

    Updating to xwork 2.0.4 release o xwork 2.0.4 contains the fix for the critical security issue   (see https://issues.apache.org/struts/browse/WW-2030 for details)
    Note: The xwork group id changed from 'opensymphony' to 'com.opensymphony'
    
    Issue Number: WW-2030
    
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/branches/STRUTS_2_0_X@557701 13f79535-47bb-0310-9956-ffa450edef68

--- backport/translate.bat
+++ backport/translate.bat
@@ -1,2 +1,2 @@
 java -jar retrotranslator-transformer-1.2.0.jar -advanced -srcjar ../lib/struts2-core-2.0.8.jar -destjar struts2-core-j4-2.0.8.jar
-java -jar retrotranslator-transformer-1.2.0.jar -advanced -srcjar ../lib/xwork-2.0.3.jar -destjar xwork-j4-2.0.3.jar 
+java -jar retrotranslator-transformer-1.2.0.jar -advanced -srcjar ../lib/xwork-2.0.4.jar -destjar xwork-j4-2.0.4.jar 

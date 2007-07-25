3d9f5789921fb77366b006f1313260e8c66d47e6
James Holmes <jholmes@apache.org>
2007-07-25 03:45:17 +0000

    WW-1911 File Upload: content-type validation fails when no file is uploaded
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@559307 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/struts2/dispatcher/multipart/JakartaMultiPartRequest.java
+++ core/src/main/java/org/apache/struts2/dispatcher/multipart/JakartaMultiPartRequest.java
@@ -115,6 +115,12 @@ public class JakartaMultiPartRequest implements MultiPartRequest {
                 } else {
                     log.debug("Item is a file upload");
 
+                    // Skip file uploads that don't have a file name - meaning that no file was selected.
+                    if (item.getName() == null || item.getName().trim().length() < 1) {
+                        log.debug("No file has been uploaded for the field: " + item.getFieldName());
+                        continue;
+                    }
+
                     List<FileItem> values;
                     if (files.get(item.getFieldName()) != null) {
                         values = files.get(item.getFieldName());

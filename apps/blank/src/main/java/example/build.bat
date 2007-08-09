266a553f5d0ddf9bb447f74b5c9c0807b9af5033
Antonio Petrelli <apetrelli@apache.org>
2007-08-09 18:59:58 +0000

    WW-2049 Reverted the license headers in sample apps code. I left only in pom.xml files and in Java code.
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@564339 13f79535-47bb-0310-9956-ffa450edef68

--- apps/blank/src/main/java/example/build.bat
+++ apps/blank/src/main/java/example/build.bat
@@ -1,22 +1,3 @@
 @echo off
-REM $Id: pom.xml 559206 2007-07-24 21:01:18Z apetrelli $
-REM
-REM Licensed to the Apache Software Foundation (ASF) under one
-REM or more contributor license agreements.  See the NOTICE file
-REM distributed with this work for additional information
-REM regarding copyright ownership.  The ASF licenses this file
-REM to you under the Apache License, Version 2.0 (the
-REM "License"); you may not use this file except in compliance
-REM with the License.  You may obtain a copy of the License at
-REM
-REM  http://www.apache.org/licenses/LICENSE-2.0
-REM
-REM Unless required by applicable law or agreed to in writing,
-REM software distributed under the License is distributed on an
-REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-REM KIND, either express or implied.  See the License for the
-REM specific language governing permissions and limitations
-REM under the License.
-
 set CLASSPATH=..\..\..\lib\xwork-2.0-beta-1.jar
 javac *.java -d ..\..\..\classes

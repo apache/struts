f72fada5cbec2bd85536f22e0490c478a723b3fc
Donald J. Brown <mrdon@apache.org>
2007-09-03 02:39:34 +0000

    Fixed selection of radio item when value is null WW-2045
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@572216 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/views/jsp/ui/RadioTest.java
+++ core/src/test/java/org/apache/struts2/views/jsp/ui/RadioTest.java
@@ -75,6 +75,28 @@ public class RadioTest extends AbstractUITagTest {
 
         verify(RadioTag.class.getResource("Radio-2.txt"));
     }
+    
+    public void testMapCheckedNull() throws Exception {
+        TestAction testAction = (TestAction) action;
+        testAction.setFoo("bar");
+
+        HashMap map = new HashMap();
+        map.put("1", "One");
+        map.put("2", "Two");
+        testAction.setMap(map);
+
+        RadioTag tag = new RadioTag();
+        tag.setPageContext(pageContext);
+        tag.setLabel("mylabel");
+        tag.setName("myname");
+        tag.setValue("%{map['3']}");
+        tag.setList("#@java.util.HashMap@{\"1\":\"One\", \"2\":\"Two\", \"\":\"N/A\"}");
+
+        tag.doStartTag();
+        tag.doEndTag();
+
+        verify(RadioTag.class.getResource("Radio-4.txt"));
+    }
 
     public void testSimple() throws Exception {
         TestAction testAction = (TestAction) action;

9375fe9c6aa446c6fb9c2acfae82522676d0da56
Donald J. Brown <mrdon@apache.org>
2007-10-20 10:04:53 +0000

    Adding test for original extension uri building WW-2267
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@586696 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/dispatcher/mapper/DefaultActionMapperTest.java
+++ core/src/test/java/org/apache/struts2/dispatcher/mapper/DefaultActionMapperTest.java
@@ -196,6 +196,17 @@ public class DefaultActionMapperTest extends StrutsTestCase {
 
         assertEquals("/my/namespace/actionName!add.action", mapper.getUriFromActionMapping(mapping));
     }
+    
+    public void testGetUriWithOriginalExtension() throws Exception {
+        ActionMapping mapping = new ActionMapping("actionName", "/ns", null, new HashMap());
+        
+        ActionMapping orig = new ActionMapping();
+        orig.setExtension("foo");
+        ActionContext.getContext().put(ServletActionContext.ACTION_MAPPING, orig);
+        
+        DefaultActionMapper mapper = new DefaultActionMapper();
+        assertEquals("/ns/actionName.foo", mapper.getUriFromActionMapping(mapping));
+    }
 
     public void testGetMappingWithNoExtension() throws Exception {
         req.setupGetParameterMap(new HashMap());

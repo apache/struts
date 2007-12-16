918a89ce7d1b07bbb8bc6d35f849d0a59394cf14
Donald J. Brown <mrdon@apache.org>
2007-12-16 10:56:47 +0000

    Moving api package to sandbox, cleaning up retrotranslator so it can be executed by CI, removing unnecessary backport directory
    WW-1700 WW-2258
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@604603 13f79535-47bb-0310-9956-ffa450edef68

--- backport/translate.bat
+++
@@ -1,3 +0,0 @@
-java -jar retrotranslator-transformer-1.2.3.jar -advanced -srcjar ../lib/struts2-core-2.1.1-SNAPSHOT.jar -destjar struts2-core-backport-2.1.1-SNAPSHOT.jar 
-java -jar retrotranslator-transformer-1.2.3.jar -advanced -srcjar ../lib/struts2-api-2.1.1-SNAPSHOT.jar -destjar struts2-api-backport-2.1.1-SNAPSHOT.jar 
-java -jar retrotranslator-transformer-1.2.3.jar -advanced -srcjar ../lib/xwork-2.1.1-SNAPSHOT.jar -destjar xwork-backport-2.1.1-SNAPSHOT.jar 

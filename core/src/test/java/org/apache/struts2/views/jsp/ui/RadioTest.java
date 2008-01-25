876cf91a3097606a5e2c33d762f70a9b17ee4a1c
Donald J. Brown <mrdon@apache.org>
2008-01-25 12:22:22 +0000

    Adding test to ensure label separator for radio tag works WW-2343
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@615201 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/views/jsp/ui/RadioTest.java
+++ core/src/test/java/org/apache/struts2/views/jsp/ui/RadioTest.java
@@ -121,6 +121,30 @@ public class RadioTest extends AbstractUITagTest {
         verify(RadioTag.class.getResource("Radio-1.txt"));
     }
 
+    public void testSimpleWithLabelSeparator() throws Exception {
+        TestAction testAction = (TestAction) action;
+        testAction.setFoo("bar");
+        testAction.setList(new String[][]{
+                {"hello", "world"},
+                {"foo", "bar"}
+        });
+
+        RadioTag tag = new RadioTag();
+        tag.setPageContext(pageContext);
+        tag.setLabel("mylabel");
+        tag.setName("myname");
+        tag.setValue("");
+        tag.setList("list");
+        tag.setListKey("top[0]");
+        tag.setListValue("top[1]");
+        tag.setLabelSeparator("--");
+
+        tag.doStartTag();
+        tag.doEndTag();
+
+        verify(RadioTag.class.getResource("Radio-5.txt"));
+    }
+
     public void testGenericSimple() throws Exception {
         RadioTag tag = new RadioTag();
         prepareTagGeneric(tag);

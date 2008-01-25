7e169a93e2772eb861d5a833e54b9ef4c031567b
Donald J. Brown <mrdon@apache.org>
2008-01-25 12:40:17 +0000

    Escaping forward slashes in element ids WW-2303
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@615208 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/components/UIBeanTest.java
+++ core/src/test/java/org/apache/struts2/components/UIBeanTest.java
@@ -85,6 +85,20 @@ public class UIBeanTest extends StrutsTestCase {
         assertEquals("formId_txtFldName", txtFld.getParameters().get("id"));
     }
 
+    public void testEscapeId() throws Exception {
+        ValueStack stack = ActionContext.getContext().getValueStack();
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        MockHttpServletResponse res = new MockHttpServletResponse();
+
+        Form form = new Form(stack, req, res);
+        form.getParameters().put("id", "formId");
+
+        TextField txtFld = new TextField(stack, req, res);
+        txtFld.setName("foo/bar");
+        txtFld.populateComponentHtmlId(form);
+        assertEquals("formId_foo_bar", txtFld.getParameters().get("id"));
+    }
+
     public void testGetThemeFromForm() throws Exception {
         ValueStack stack = ActionContext.getContext().getValueStack();
         MockHttpServletRequest req = new MockHttpServletRequest();

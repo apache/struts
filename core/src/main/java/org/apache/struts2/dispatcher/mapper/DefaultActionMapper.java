c897c038102b4bc108012c4249209070535cacbe
Brian Pontarelli <bpontarelli@apache.org>
2008-02-05 20:49:54 +0000

    Fixed bug in extensionless URL handling
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@618772 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/struts2/dispatcher/mapper/DefaultActionMapper.java
+++ core/src/main/java/org/apache/struts2/dispatcher/mapper/DefaultActionMapper.java
@@ -453,7 +453,7 @@ public class DefaultActionMapper implements ActionMapper {
                 // This should also handle cases such as /foo/bar-1.0/description. It is tricky to
                 // distinquish /foo/bar-1.0 but perhaps adding a numeric check in the future could
                 // work
-                int index = name.indexOf('.');
+                int index = name.lastIndexOf('.');
                 if (index == -1 || name.indexOf('/', index) >= 0) {
                     return name;
                 }

69604a125124052127d9a632315ff6d48298f66f
Donald J. Brown <mrdon@apache.org>
2008-02-19 08:35:35 +0000

    Adding test to ensure slashes in names works with root namespace WW-2461
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@629022 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/dispatcher/mapper/DefaultActionMapperTest.java
+++ core/src/test/java/org/apache/struts2/dispatcher/mapper/DefaultActionMapperTest.java
@@ -115,6 +115,22 @@ public class DefaultActionMapperTest extends StrutsTestCase {
         assertNull(mapping.getMethod());
     }
 
+    public void testGetMappingWithSlashedNameAtRoot() throws Exception {
+
+        req.setupGetRequestURI("/foo/actionName.action");
+        req.setupGetServletPath("/foo/actionName.action");
+        req.setupGetAttribute(null);
+        req.addExpectedGetAttributeName("javax.servlet.include.servlet_path");
+
+        DefaultActionMapper mapper = new DefaultActionMapper();
+        mapper.setSlashesInActionNames("true");
+        ActionMapping mapping = mapper.getMapping(req, configManager);
+
+        assertEquals("", mapping.getNamespace());
+        assertEquals("foo/actionName", mapping.getName());
+        assertNull(mapping.getMethod());
+    }
+
     public void testGetMappingWithNamespaceSlash() throws Exception {
 
         req.setupGetRequestURI("/my.hh/abc.action");

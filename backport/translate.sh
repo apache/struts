9f556fdbac94f50d66994d2419303e70bb50a339
René Gielen <rgielen@apache.org>
2008-03-02 14:01:14 +0000

    WW-2524: setting 2.0.11.1 version labels for upcoming release
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/branches/STRUTS_2_0_X@632759 13f79535-47bb-0310-9956-ffa450edef68

--- backport/translate.sh
+++ backport/translate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar retrotranslator-transformer-1.2.2.jar -advanced -srcjar ../lib/struts2-core-2.0.10.jar -destjar struts2-core-j4-2.0.10.jar
+java -jar retrotranslator-transformer-1.2.2.jar -advanced -srcjar ../lib/struts2-core-2.0.11.1.jar -destjar struts2-core-j4-2.0.11.1.jar
 java -jar retrotranslator-transformer-1.2.2.jar -advanced -srcjar ../lib/xwork-2.0.4.jar -destjar xwork-j4-2.0.4.jar 

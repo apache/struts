52b6a00e378632d772d1b4f64d1b443705be17a5
René Gielen <rgielen@apache.org>
2008-06-22 16:55:23 +0000

    WW-2693: Preparing security fix release 2.0.11.2
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/branches/STRUTS_2_0_X@670383 13f79535-47bb-0310-9956-ffa450edef68

--- backport/translate.bat
+++ backport/translate.bat
@@ -1,2 +1,2 @@
-java -jar retrotranslator-transformer-1.2.2.jar -advanced -srcjar ../lib/struts2-core-2.0.11.1.jar -destjar struts2-core-j4-2.0.11.1.jar
+java -jar retrotranslator-transformer-1.2.2.jar -advanced -srcjar ../lib/struts2-core-2.0.11.2.jar -destjar struts2-core-j4-2.0.11.2.jar
 java -jar retrotranslator-transformer-1.2.2.jar -advanced -srcjar ../lib/xwork-2.0.5.jar -destjar xwork-j4-2.0.5.jar 

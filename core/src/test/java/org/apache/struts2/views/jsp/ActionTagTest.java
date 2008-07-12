a4ee229d4318db051c2801db497e03b411d11b44
Donald J. Brown <mrdon@apache.org>
2008-07-12 13:55:51 +0000

    Adding test to ensure action tags are executed without the method from the URI polluting the result WW-2290
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@676179 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/views/jsp/ActionTagTest.java
+++ core/src/test/java/org/apache/struts2/views/jsp/ActionTagTest.java
@@ -106,6 +106,35 @@ public class ActionTagTest extends AbstractTagTest {
         this.testSimple();
     }
 
+    public void testSimpleWithctionMethodInOriginalURI() {
+        request.setupGetServletPath("/foo!foo.action");
+
+        ActionTag tag = new ActionTag();
+        tag.setPageContext(pageContext);
+        tag.setName("testAction");
+        tag.setId("testAction");
+
+        int stackSize = stack.size();
+
+        try {
+            tag.doStartTag();
+            tag.addParameter("foo", "myFoo");
+            tag.doEndTag();
+
+            assertEquals(stack.size(), ActionContext.getContext().getValueStack().size());
+            assertEquals("myFoo", stack.findValue("#testAction.foo"));
+            assertEquals(stackSize, stack.size());
+
+            Object o = pageContext.findAttribute("testAction");
+            assertTrue(o instanceof TestAction);
+            assertEquals("myFoo", ((TestAction) o).getFoo());
+            assertEquals(Action.SUCCESS, ((TestAction) o).getResult());
+        } catch (JspException ex) {
+            ex.printStackTrace();
+            fail();
+        }
+    }
+
     public void testActionWithExecuteResult() throws Exception {
         ActionTag tag = new ActionTag();
         tag.setPageContext(pageContext);

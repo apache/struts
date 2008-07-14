f2ff3a81d247166b3ff3d862a1e163938995d63a
Donald J. Brown <mrdon@apache.org>
2008-07-14 12:50:38 +0000

    Adding test for missing xslt include WW-2561
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@676575 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/views/xslt/XSLTResultTest.java
+++ core/src/test/java/org/apache/struts2/views/xslt/XSLTResultTest.java
@@ -153,6 +153,28 @@ public class XSLTResultTest extends StrutsTestCase {
         assertTrue(out.startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"));
         assertTrue(out.indexOf("<validators>") > -1);
     }
+
+    public void testTransform4WithBadDocumentInclude() throws Exception {
+        result = new XSLTResult(){
+            protected URIResolver getURIResolver() {
+                return new URIResolver() {
+                    public Source resolve(String href, String base) throws TransformerException {
+                        return new StreamSource(ClassLoaderUtils.getResourceAsStream(href, this.getClass()));
+                    }
+
+                };
+            }
+
+        };
+        result.setParse(false);
+        result.setLocation("XSLTResultTest4.badinclude.xsl");
+        try {
+            result.execute(mai);
+            fail("Should have thrown an exception");
+        } catch (Exception ex) {
+            assertEquals("Error transforming result", ex.getMessage());
+        }
+    }
     
     public void testTransformWithError() throws Exception {
         result = new XSLTResult(){

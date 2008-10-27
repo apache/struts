59d0cc5e562e43badbcde043c0353a690e134a9b
René Gielen <rgielen@apache.org>
2008-10-27 22:36:20 +0000

    WW-2852: Prepare 2.0.13 release
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/branches/STRUTS_2_0_X@708360 13f79535-47bb-0310-9956-ffa450edef68

--- backport/translate.sh
+++ backport/translate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar retrotranslator-transformer-1.2.2.jar -advanced -srcjar ../lib/struts2-core-2.0.12.jar -destjar struts2-core-j4-2.0.12.jar
+java -jar retrotranslator-transformer-1.2.2.jar -advanced -srcjar ../lib/struts2-core-2.0.13.jar -destjar struts2-core-j4-2.0.13.jar
 java -jar retrotranslator-transformer-1.2.2.jar -advanced -srcjar ../lib/xwork-2.0.6.jar -destjar xwork-j4-2.0.6.jar 

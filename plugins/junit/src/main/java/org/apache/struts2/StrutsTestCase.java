976863cfb7582c2d860cd4a49953356b60963cc7
Musachy Barroso <musachy@apache.org>
2009-08-07 18:11:11 +0000

    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@802124 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/junit/src/main/java/org/apache/struts2/StrutsTestCase.java
+++ plugins/junit/src/main/java/org/apache/struts2/StrutsTestCase.java
@@ -24,25 +24,48 @@ package org.apache.struts2;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
+import java.io.UnsupportedEncodingException;
 
 import org.apache.struts2.dispatcher.Dispatcher;
+import org.apache.struts2.dispatcher.mapper.ActionMapping;
+import org.apache.struts2.dispatcher.mapper.ActionMapper;
 import org.apache.struts2.util.StrutsTestCaseHelper;
 import org.springframework.mock.web.MockServletContext;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockPageContext;
+import org.springframework.core.io.DefaultResourceLoader;
 
 import com.opensymphony.xwork2.XWorkTestCase;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionProxyFactory;
+import com.opensymphony.xwork2.ActionProxy;
+import com.opensymphony.xwork2.config.Configuration;
 import com.opensymphony.xwork2.util.logging.LoggerFactory;
 import com.opensymphony.xwork2.util.logging.jdk.JdkLoggerFactory;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletException;
 
 /**
  * Base test case for JUnit testing Struts.
  */
 public abstract class StrutsTestCase extends XWorkTestCase {
+    protected MockHttpServletResponse response;
+    protected MockHttpServletRequest request;
+    protected MockPageContext pageContext;
+    protected MockServletContext servletContext;
+    protected Map<String, String> dispatcherInitParams;
+    
+    protected DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
 
     static {
         ConsoleHandler handler = new ConsoleHandler();
@@ -53,7 +76,7 @@ public abstract class StrutsTestCase extends XWorkTestCase {
                 StringBuilder sb = new StringBuilder();
                 sb.append(record.getLevel());
                 sb.append(':');
-                for (int x=9-record.getLevel().toString().length(); x>0; x--) {
+                for (int x = 9 - record.getLevel().toString().length(); x > 0; x--) {
                     sb.append(' ');
                 }
                 sb.append('[');
@@ -67,23 +90,118 @@ public abstract class StrutsTestCase extends XWorkTestCase {
         handler.setFormatter(formatter);
         Logger logger = Logger.getLogger("");
         if (logger.getHandlers().length > 0)
-            logger.removeHandler(logger.getHandlers ()[0]);
+            logger.removeHandler(logger.getHandlers()[0]);
         logger.addHandler(handler);
         logger.setLevel(Level.WARNING);
         LoggerFactory.setLoggerFactory(new JdkLoggerFactory());
     }
-    
+
+    /**
+     * gets an object from the stack after an action is executed
+     */
+    protected Object findValueAfterExecute(String key) {
+        return ServletActionContext.getValueStack(request).findValue(key);
+    }
+
+    /**
+     * Executes an action and returns it's output (not the result returned from
+     * execute()), but the actual output that would be written to the response.
+     * For this to work the configured result for the action needs to be
+     * FreeMarker, or Velocity (actually, anything except JSPs)
+     */
+    protected String executeAction(String uri) throws ServletException, UnsupportedEncodingException {
+        request.setRequestURI(uri);
+        ActionMapping mapping = getActionMapping(request);
+
+        assertNotNull(mapping);
+        Dispatcher.getInstance().serviceAction(request, response, servletContext, mapping);
+
+        if (response.getStatus() != HttpServletResponse.SC_OK)
+            throw new ServletException("Error code [" + response.getStatus() + "], Error: ["
+                    + response.getErrorMessage() + "]");
+
+        return response.getContentAsString();
+    }
+
+    /**
+     * Creates an action profy for a request, and sets parameters of the ActionInvocation to the passed
+     * parameters. Make sure to set the request parameters in the protected "request" object before calling this method.
+     */
+    protected ActionProxy getActionProxy(String uri) {
+        request.setRequestURI(uri);
+        ActionMapping mapping = getActionMapping(request);
+        String namespace = mapping.getNamespace();
+        String name = mapping.getName();
+        String method = mapping.getMethod();
+
+        Configuration config = configurationManager.getConfiguration();
+        ActionProxy proxy = config.getContainer().getInstance(ActionProxyFactory.class).createActionProxy(
+                namespace, name, method, new HashMap<String, Object>(), true, false);
+
+        ActionContext invocationContext = proxy.getInvocation().getInvocationContext();
+        invocationContext.setParameters(new HashMap(request.getParameterMap()));
+        // set the action context to the one used by the proxy
+        ActionContext.setContext(invocationContext);
+
+        // this is normaly done in onSetUp(), but we are using Struts internal
+        // objects (proxy and action invocation)
+        // so we have to hack around so it works
+        ServletActionContext.setServletContext(servletContext);
+        ServletActionContext.setRequest(request);
+        ServletActionContext.setResponse(response);
+
+        return proxy;
+    }
+
+    /**
+     * Finds an an ActionMapping for a given request
+     */
+    protected ActionMapping getActionMapping(HttpServletRequest request) {
+        return Dispatcher.getInstance().getContainer().getInstance(ActionMapper.class).getMapping(request,
+                Dispatcher.getInstance().getConfigurationManager());
+    }
+
+    /**
+     * Finds an an ActionMapping for a given url
+     */
+    protected ActionMapping getActionMapping(String url) {
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        req.setRequestURI(url);
+        return Dispatcher.getInstance().getContainer().getInstance(ActionMapper.class).getMapping(req,
+                Dispatcher.getInstance().getConfigurationManager());
+    }
+
+    /**
+     * Injects dependencies on an Object using Struts internal IoC container
+     */
+    protected void injectStrutsDependencies(Object object) {
+        Dispatcher.getInstance().getContainer().inject(object);
+    }
+
+
     /**
      * Sets up the configuration settings, XWork configuration, and
      * message resources
      */
     protected void setUp() throws Exception {
         super.setUp();
-        initDispatcher(null);
+        initServletMockObjects();
+        setupBeforeInitDispatcher();
+        initDispatcher(dispatcherInitParams);
     }
-    
-    protected Dispatcher initDispatcher(Map<String,String> params) {
-        Dispatcher du = StrutsTestCaseHelper.initDispatcher(new MockServletContext(), params);
+
+    protected void setupBeforeInitDispatcher() throws Exception {
+    }
+
+    protected void initServletMockObjects() {
+        servletContext = new MockServletContext(resourceLoader);
+        response = new MockHttpServletResponse();
+        request = new MockHttpServletRequest();
+        pageContext = new MockPageContext(servletContext, request, response);
+    }
+
+    protected Dispatcher initDispatcher(Map<String, String> params) {
+        Dispatcher du = StrutsTestCaseHelper.initDispatcher(servletContext, params);
         configurationManager = du.getConfigurationManager();
         configuration = configurationManager.getConfiguration();
         container = configuration.getContainer();

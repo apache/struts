31fde5a0677a67f73aebafb8bc744423389b1860
Musachy Barroso <musachy@apache.org>
2009-08-10 23:30:03 +0000

    WW-2807  Improve Velocity tag syntax and performance
    thanks to Christopher Schultz for patch
    
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@802956 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/struts2/views/velocity/components/AbstractDirective.java
+++ core/src/main/java/org/apache/struts2/views/velocity/components/AbstractDirective.java
@@ -94,15 +94,34 @@ public abstract class AbstractDirective extends Directive {
      *          if the was an error in the format of the property
      */
     protected Map createPropertyMap(InternalContextAdapter contextAdapter, Node node) throws ParseErrorException, MethodInvocationException {
-        Map propertyMap = new HashMap();
+        Map propertyMap;
 
         int children = node.jjtGetNumChildren();
         if (getType() == BLOCK) {
             children--;
         }
 
-        for (int index = 0, length = children; index < length; index++) {
-            this.putProperty(propertyMap, contextAdapter, node.jjtGetChild(index));
+        // Velocity supports an on-the-fly Map-definition syntax that leads
+        // to more readable and faster code:
+        //
+        //    #url({'id':'url', 'action':'MyAction'})
+        //
+        // We support this syntax by checking for a single Map argument
+        // to any directive and using that as the property map instead
+        // of building one from individual name-value pair strings.
+        Node firstChild = null;
+        Object firstValue = null;
+        if(children == 1
+           && null != (firstChild = node.jjtGetChild(0))
+           && null != (firstValue = firstChild.value(contextAdapter))
+           && firstValue instanceof Map) {
+            propertyMap = (Map)firstValue;
+        } else {
+            propertyMap = new HashMap();
+
+            for (int index = 0, length = children; index < length; index++) {
+                this.putProperty(propertyMap, contextAdapter, node.jjtGetChild(index));
+            }
         }
 
         return propertyMap;

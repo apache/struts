535bc0163d71478633103f11bca708118c39ee62
René Gielen <rgielen@apache.org>
2011-04-23 07:46:43 +0000

    code cleanup
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@1096118 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/struts2/components/template/JspTemplateEngineTest.java
+++ core/src/test/java/org/apache/struts2/components/template/JspTemplateEngineTest.java
@@ -2,16 +2,11 @@ package org.apache.struts2.components.template;
 
 import org.apache.struts2.StrutsTestCase;
 
-/**
- * JspTemplateEngineTest.
- *
- * @author Rene Gielen
- */
 public class JspTemplateEngineTest extends StrutsTestCase {
 
-	public void testEncodingGetsInjected() throws Exception {
-		JspTemplateEngine jspTemplateEngine = new JspTemplateEngine();
-		container.inject(jspTemplateEngine);
-		assertNotNull(jspTemplateEngine.encoding);
-	}
+    public void testEncodingGetsInjected() throws Exception {
+        JspTemplateEngine jspTemplateEngine = new JspTemplateEngine();
+        container.inject(jspTemplateEngine);
+        assertNotNull(jspTemplateEngine.encoding);
+    }
 }

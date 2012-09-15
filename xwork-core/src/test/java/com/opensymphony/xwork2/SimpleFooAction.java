76ff5832b54e2b535c9d1a7ce1b3c1872c29185c
Lukasz Lenart <lukaszlenart@apache.org>
2012-09-15 06:50:08 +0000

    WW-3876 adds additional test cases for NumberConverter
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@1385022 13f79535-47bb-0310-9956-ffa450edef68

--- xwork-core/src/test/java/com/opensymphony/xwork2/SimpleFooAction.java
+++ xwork-core/src/test/java/com/opensymphony/xwork2/SimpleFooAction.java
@@ -24,7 +24,18 @@ package com.opensymphony.xwork2;
  */
 public class SimpleFooAction implements Action {
 
+    private Integer id;
+
     public String execute() throws Exception {
         return SUCCESS;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
 }

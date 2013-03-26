3bf95951bc2023be25c14c449a9ea3c04565a2e9
Lukasz Lenart <lukaszlenart@apache.org>
2013-03-26 06:21:53 +0000

    WW-4025 Solves problem with NPE in TextParseUtil
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@1460985 13f79535-47bb-0310-9956-ffa450edef68

--- xwork-core/src/main/java/com/opensymphony/xwork2/util/TextParseUtil.java
+++ xwork-core/src/main/java/com/opensymphony/xwork2/util/TextParseUtil.java
@@ -153,17 +153,15 @@ public class TextParseUtil {
      */
     public static Object translateVariables(char[] openChars, String expression, final ValueStack stack, final Class asType, final ParsedValueEvaluator evaluator, int maxLoopCount) {
 
-
         ParsedValueEvaluator ognlEval = new ParsedValueEvaluator() {
             public Object evaluate(String parsedValue) {
                 Object o = stack.findValue(parsedValue, asType);
-                if (evaluator != null) {
+                if (evaluator != null && o != null) {
                     o = evaluator.evaluate(o.toString());
                 }
                 return o;
             }
         };
-
 
         TextParser parser = ((Container)stack.getContext().get(ActionContext.CONTAINER)).getInstance(TextParser.class);
 
--- xwork-core/src/test/java/com/opensymphony/xwork2/util/TextParseUtilTest.java
+++ xwork-core/src/test/java/com/opensymphony/xwork2/util/TextParseUtilTest.java
@@ -138,4 +138,22 @@ public class TextParseUtilTest extends XWorkTestCase {
         assertEquals("foo: 2", s);
     }
 
+    public void testTranslateVariablesWithNull() {
+        // given
+        ValueStack stack = ActionContext.getContext().getValueStack();
+        stack.push(new HashMap<String, Object>() {{ put("foo", null); }});
+
+        TextParseUtil.ParsedValueEvaluator evaluator = new TextParseUtil.ParsedValueEvaluator() {
+            public Object evaluate(String parsedValue) {
+                return parsedValue;
+            }
+        };
+
+        // when
+        Object s = TextParseUtil.translateVariables('$', "foo: ${foo}", stack, String.class, evaluator, 2);
+
+        // then
+        assertEquals("foo: ", s);
+    }
+
 }

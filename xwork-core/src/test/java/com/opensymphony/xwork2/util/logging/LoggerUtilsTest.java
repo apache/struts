0f5a74b98357ad6a6d94d1ae13a0cbdd478fb4d0
Christian Grobmeier <grobmeier@apache.org>
2013-06-13 08:51:05 +0000

    testing expression enclosed by characters without space
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@1492574 13f79535-47bb-0310-9956-ffa450edef68

--- xwork-core/src/test/java/com/opensymphony/xwork2/util/logging/LoggerUtilsTest.java
+++ xwork-core/src/test/java/com/opensymphony/xwork2/util/logging/LoggerUtilsTest.java
@@ -13,7 +13,9 @@ public class LoggerUtilsTest extends TestCase {
         assertEquals("foo bob", LoggerUtils.format("foo #0", "bob"));
         assertEquals("foo bob joe", LoggerUtils.format("foo #0 #1", "bob", "joe"));
         assertEquals("foo bob joe #8", LoggerUtils.format("foo #0 #1 #8", "bob", "joe"));
-        
+        assertEquals("foo (bob/ally)", LoggerUtils.format("foo (#0/#1)", "bob", "ally"));
+        assertEquals("foo (bobally)", LoggerUtils.format("foo (#0#1)", "bob", "ally"));
+
         assertEquals(null, LoggerUtils.format(null));
         assertEquals("", LoggerUtils.format(""));
         

3568814288c54b2886ea8ac5f10ad3da5410ecb1
Lukasz Lenart <lukaszlenart@apache.org>
2013-11-20 08:03:09 +0000

    WW-4243 Adds missing BeanSelectionProvider interface
    git-svn-id: https://svn.apache.org/repos/asf/struts/struts2/trunk@1543731 13f79535-47bb-0310-9956-ffa450edef68

---
+++ xwork-core/src/main/java/com/opensymphony/xwork2/config/BeanSelectionProvider.java
@@ -0,0 +1,8 @@
+package com.opensymphony.xwork2.config;
+
+/**
+ * When implemented allows to alias already existing beans
+ */
+public interface BeanSelectionProvider extends ConfigurationProvider {
+
+}

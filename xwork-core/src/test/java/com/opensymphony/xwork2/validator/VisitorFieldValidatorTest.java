46f7f6c7a87cbf70e89c5d30e1155e897f759b09
Aleksandr Mashchenko <aleksandr.7m@gmail.com>
2014-05-16 23:57:29 +0300

    tests for AppendingValidatorContext and nested visitor conversion validation

--- xwork-core/src/test/java/com/opensymphony/xwork2/validator/VisitorFieldValidatorTest.java
+++ xwork-core/src/test/java/com/opensymphony/xwork2/validator/VisitorFieldValidatorTest.java
@@ -179,7 +179,32 @@ public class VisitorFieldValidatorTest extends XWorkTestCase {
         //the error from the action should be there too
         assertTrue(fieldErrors.containsKey("context"));
     }
-    
+
+    public void testVisitorChildConversionValidation() throws Exception {
+        //add conversion error
+        Map<String, Object> conversionErrors = new HashMap<String, Object>();
+        conversionErrors.put("bean.child.count", "bar");
+        ActionContext.getContext().setConversionErrors(conversionErrors);
+
+        validate("visitorChildValidation");
+        assertTrue(action.hasFieldErrors());
+
+        Map<String, List<String>> fieldErrors = action.getFieldErrors();
+        assertEquals(6, fieldErrors.size());
+        assertTrue(!fieldErrors.containsKey("bean.count"));
+        assertTrue(fieldErrors.containsKey("bean.name"));
+        assertTrue(fieldErrors.containsKey("bean.birth"));
+
+        assertTrue(fieldErrors.containsKey("bean.child.name"));
+        assertTrue(fieldErrors.containsKey("bean.child.birth"));
+
+        //the error from the action should be there too
+        assertTrue(fieldErrors.containsKey("context"));
+
+        //nested visitor conversion error
+        assertTrue(fieldErrors.containsKey("bean.child.count"));
+    }
+
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();

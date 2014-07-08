19b939cfbd7960e05b960fad7c3882cc60d35e72
Lukasz Lenart <lukaszlenart@apache.org>
2014-07-08 22:16:53 +0200

    WW-3485 Adds additional test case to proof that underscores are accepted

--- xwork-core/src/test/java/com/opensymphony/xwork2/security/DefaultAcceptedPatternsCheckerTest.java
+++ xwork-core/src/test/java/com/opensymphony/xwork2/security/DefaultAcceptedPatternsCheckerTest.java
@@ -53,4 +53,15 @@ public class DefaultAcceptedPatternsCheckerTest extends XWorkTestCase {
         }
     }
 
+    public void testUnderscoreInParamName() throws Exception {
+        // given
+        AcceptedPatternsChecker checker = new DefaultAcceptedPatternsChecker();
+
+        // when
+        AcceptedPatternsChecker.IsAccepted actual = checker.isAccepted("mapParam['param_1']");
+
+        // then
+        assertTrue("Param with underscore wasn't accepted!", actual.isAccepted());
+    }
+
 }

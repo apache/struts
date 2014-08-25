bf0826f591ee0a9c9a531ed42ec77dc7590a3a58
Lukasz Lenart <lukaszlenart@apache.org>
2014-08-25 10:33:15 +0200

    WW-4390 Adds additional use case to proof that everything works

--- xwork-core/src/test/java/com/opensymphony/xwork2/security/DefaultExcludedPatternsCheckerTest.java
+++ xwork-core/src/test/java/com/opensymphony/xwork2/security/DefaultExcludedPatternsCheckerTest.java
@@ -83,4 +83,21 @@ public class DefaultExcludedPatternsCheckerTest extends XWorkTestCase {
         }
     }
 
+    public void testStrutsTokenIsExcluded() throws Exception {
+        // given
+        List<String> tokens = new ArrayList<String>();
+        tokens.add("struts.token.name");
+        tokens.add("struts.token");
+
+        ExcludedPatternsChecker checker = new DefaultExcludedPatternsChecker();
+
+        for (String token : tokens) {
+            // when
+            ExcludedPatternsChecker.IsExcluded actual = checker.isExcluded(token);
+
+            // then
+            assertTrue("Param '" + token + "' is not excluded!", actual.isExcluded());
+        }
+    }
+
 }

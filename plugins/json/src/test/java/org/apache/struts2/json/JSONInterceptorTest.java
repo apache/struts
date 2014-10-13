61cffe68feda8a4c527d085647e9567b2f689fc7
Aleksandr Mashchenko <aleksandr.7m@gmail.com>
2014-10-13 20:07:07 +0300

    improved interceptor that accepts and maps JSON array

--- plugins/json/src/test/java/org/apache/struts2/json/JSONInterceptorTest.java
+++ plugins/json/src/test/java/org/apache/struts2/json/JSONInterceptorTest.java
@@ -459,7 +459,59 @@ public class JSONInterceptorTest extends StrutsTestCase {
         assertEquals(bean2.getDoubleField(), 10.1);
         assertEquals(bean2.getByteField(), 3);
     }
+    
+    public void testJSONArray() throws Exception {
+        setRequestContent("json-12.txt");
+        this.request.addHeader("content-type", "application/json");
 
+        // interceptor
+        JSONInterceptor interceptor = new JSONInterceptor();
+        interceptor.setRoot("beans");
+        TestAction5 action = new TestAction5();
+
+        this.invocation.setAction(action);
+        this.invocation.getStack().push(action);
+
+        interceptor.intercept(this.invocation);
+
+        List<Bean> beans = action.getBeans();
+
+        assertNotNull(beans);
+        assertEquals(1, beans.size());
+        assertTrue(beans.get(0).isBooleanField());
+        assertEquals(beans.get(0).getStringField(), "test");
+        assertEquals(beans.get(0).getIntField(), 10);
+        assertEquals(beans.get(0).getCharField(), 's');
+        assertEquals(beans.get(0).getDoubleField(), 10.1);
+        assertEquals(beans.get(0).getByteField(), 3);
+    }
+
+    public void testJSONArray2() throws Exception {
+        setRequestContent("json-12.txt");
+        this.request.addHeader("content-type", "application/json");
+
+        // interceptor
+        JSONInterceptor interceptor = new JSONInterceptor();
+        interceptor.setRoot("anotherBean.yetAnotherBean.beans");
+        TestAction5 action = new TestAction5();
+
+        this.invocation.setAction(action);
+        this.invocation.getStack().push(action);
+
+        interceptor.intercept(this.invocation);
+
+        List<Bean> beans = action.getAnotherBean().getYetAnotherBean().getBeans();
+
+        assertNotNull(beans);
+        assertEquals(1, beans.size());
+        assertTrue(beans.get(0).isBooleanField());
+        assertEquals(beans.get(0).getStringField(), "test");
+        assertEquals(beans.get(0).getIntField(), 10);
+        assertEquals(beans.get(0).getCharField(), 's');
+        assertEquals(beans.get(0).getDoubleField(), 10.1);
+        assertEquals(beans.get(0).getByteField(), 3);
+    }
+    
     @Override
     protected void setUp() throws Exception {
         super.setUp();

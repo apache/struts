86da1ea8c2fec919bb3c94ceeb9c29f1acf04276
Aaron Johnson <aaron.johnson@pearson.com>
2015-06-17 09:51:25 -0500

    WW-4515 Convert try blocks to try-with-resources

--- plugins/jasperreports/src/main/java/org/apache/struts2/views/jasperreports/JasperReportsResult.java
+++ plugins/jasperreports/src/main/java/org/apache/struts2/views/jasperreports/JasperReportsResult.java
@@ -396,7 +396,7 @@ public class JasperReportsResult extends StrutsResultSupport implements JasperRe
      */
     private void writeReport(HttpServletResponse response, ByteArrayOutputStream output) throws ServletException {
         try (OutputStream outputStream = response.getOutputStream()) {
-            output.writeTo(output);
+            output.writeTo(outputStream);
             outputStream.flush();
         } catch (IOException e) {
             LOG.error("Error writing report output", e);

31af5842e022c5cb755fd697e08e655765fae981
Lukasz Lenart <lukaszlenart@apache.org>
2015-06-17 23:07:59 +0200

    Merges xwork packages into struts

--- xwork-core/src/test/java/com/opensymphony/xwork2/util/annotation/Dummy2Class.java
+++
@@ -1,9 +0,0 @@
-package com.opensymphony.xwork2.util.annotation;
-
-public class Dummy2Class {
-
-    @MyAnnotation("class-test")
-    public void methodWithAnnotation() {
-    }
-
-}
--- xwork-core/src/test/java/com/opensymphony/xwork2/util/annotation/DummyClassExt.java
+++
@@ -1,9 +0,0 @@
-package com.opensymphony.xwork2.util.annotation;
-
-public final class DummyClassExt extends DummyClass {
-
-    @MyAnnotation2
-    public void anotherAnnotatedMethod() {
-    }
-
-}

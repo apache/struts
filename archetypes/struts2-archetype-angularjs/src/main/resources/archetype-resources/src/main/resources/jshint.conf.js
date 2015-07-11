4969750f37aaae5b6adf82999066dbc58ca43811
Johannes Geppert <jogep@apache.org>
2015-07-11 10:45:31 +0200

    WW-4522 Support latest stable AngularJS version in maven angularjs archetype
    - Add maven plugin for automatic jshint execution on build

---
+++ archetypes/struts2-archetype-angularjs/src/main/resources/archetype-resources/src/main/resources/jshint.conf.js
@@ -0,0 +1,13 @@
+{
+    "maxparams": 3,
+    "indent": true,
+    "camelcase": true,
+    "eqeqeq": true,
+    "forin": true,
+    "immed": true,
+    "latedef": false,
+    "noarg": true,
+    "noempty": true,
+    "nonew": true,
+    "globals": {}
+}

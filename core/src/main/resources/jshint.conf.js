49c43b377e9f7de91dc474a6d191775126084343
Johannes Geppert <jogep@apache.org>
2015-08-04 22:00:16 +0200

    WW-4532 Javascript generated by s:doubleselect has global variables that can interfer with the rest of the app
    - Introduce new build plugin to automatically execute jslint checks during build on javascript resources

---
+++ core/src/main/resources/jshint.conf.js
@@ -0,0 +1,13 @@
+{
+    "maxparams": 5,
+    "indent": true,
+    "camelcase": true,
+    "eqeqeq": true,
+    "forin": true,
+    "immed": true,
+    "latedef": false,
+    "noarg": true,
+    "noempty": true,
+    "nonew": true,
+    "globals": {}
+}

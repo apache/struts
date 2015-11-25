8d1a1b1ce1ec598471d441f4fb8fbe4ac0a2407d
Lukasz Lenart <lukasz.lenart@gmail.com>
2015-11-25 19:03:30 +0100

    Adjusts usage of parameters

--- core/src/main/java/org/apache/struts2/interceptor/MessageStoreInterceptor.java
+++ core/src/main/java/org/apache/struts2/interceptor/MessageStoreInterceptor.java
@@ -27,6 +27,7 @@ import com.opensymphony.xwork2.interceptor.ValidationAware;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.struts2.dispatcher.HttpParameters;
 
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.dispatcher.HttpParameters;

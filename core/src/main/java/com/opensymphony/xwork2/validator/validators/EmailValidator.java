7038701026798b3b9e7ddc9008a666f45fe6f56b
Lukasz Lenart <lukaszlenart@apache.org>
2015-11-28 16:36:04 +0100

    WW-4569 Adds support for .tech

--- core/src/main/java/com/opensymphony/xwork2/validator/validators/EmailValidator.java
+++ core/src/main/java/com/opensymphony/xwork2/validator/validators/EmailValidator.java
@@ -80,7 +80,7 @@ public class EmailValidator extends RegexFieldValidator {
 
 	// see XW-371 
     public static final String EMAIL_ADDRESS_PATTERN =
-    	"\\b^['_a-z0-9-\\+]+(\\.['_a-z0-9-\\+]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*\\.([a-z]{2}|aero|arpa|asia|biz|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|nato|net|org|pro|tel|travel|xxx)$\\b";
+    	"\\b^['_a-z0-9-\\+]+(\\.['_a-z0-9-\\+]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*\\.([a-z]{2}|aero|arpa|asia|biz|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|nato|net|org|pro|tel|travel|xxx|tech)$\\b";
 
     public EmailValidator() {
         setRegex(EMAIL_ADDRESS_PATTERN);

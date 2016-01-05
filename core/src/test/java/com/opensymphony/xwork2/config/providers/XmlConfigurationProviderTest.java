dad437bd06a015c673157097d41c7c5f25b7f84f
Lukasz Lenart <lukaszlenart@apache.org>
2016-01-05 08:16:26 +0100

    WW-3710 Adds test to proof support for wildcards in <include/>

--- core/src/test/java/com/opensymphony/xwork2/config/providers/XmlConfigurationProviderTest.java
+++ core/src/test/java/com/opensymphony/xwork2/config/providers/XmlConfigurationProviderTest.java
@@ -31,6 +31,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 
 public class XmlConfigurationProviderTest extends ConfigurationTestBase {
@@ -198,4 +199,19 @@ public class XmlConfigurationProviderTest extends ConfigurationTestBase {
         assertTrue(!provider.needsReload());
     }
 
+    public void testIncludeWithWildcard() throws Exception {
+        String configFile = "com/opensymphony/xwork2/config/providers/xwork-test-include-wildcard.xml";
+        buildConfigurationProvider(configFile);
+
+        Set<String> loadedFileNames = configuration.getLoadedFileNames();
+        assertEquals(8, loadedFileNames.size());
+        assertTrue(loadedFileNames.contains("com/opensymphony/xwork2/config/providers/xwork-include-after-package.xml"));
+        assertTrue(loadedFileNames.contains("com/opensymphony/xwork2/config/providers/xwork-include-after-package-2.xml"));
+        assertTrue(loadedFileNames.contains("com/opensymphony/xwork2/config/providers/xwork-include-before-package.xml"));
+        assertTrue(loadedFileNames.contains("com/opensymphony/xwork2/config/providers/xwork-include-before-package-2.xml"));
+        assertTrue(loadedFileNames.contains("com/opensymphony/xwork2/config/providers/xwork-include-parent.xml"));
+        assertTrue(loadedFileNames.contains("com/opensymphony/xwork2/config/providers/xwork-test-include-wildcard.xml"));
+        assertTrue(loadedFileNames.contains("xwork-test-beans.xml"));
+        assertTrue(loadedFileNames.contains("xwork-test-default.xml"));
+    }
 }

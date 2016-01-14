e0003f0471f98d5986844cd799555618aae88fce
Aleksandr Mashchenko <amashchenko@apache.org>
2016-01-14 20:01:57 +0200

    WW-4585 Struts2 Rest plugin doesn't handle JSESSIONID with DMI

--- plugins/rest/src/main/java/org/apache/struts2/rest/RestActionMapper.java
+++ plugins/rest/src/main/java/org/apache/struts2/rest/RestActionMapper.java
@@ -308,9 +308,20 @@ public class RestActionMapper extends DefaultActionMapper {
     private void handleDynamicMethodInvocation(ActionMapping mapping, String name) {
         int exclamation = name.lastIndexOf("!");
         if (exclamation != -1) {
-            mapping.setName(name.substring(0, exclamation));
+            String actionName = name.substring(0, exclamation);
+            String actionMethod = name.substring(exclamation + 1);
+
+            // WW-4585
+            // add any ; appendix to name, it will be handled later in getMapping method
+            int scPos = actionMethod.indexOf(';');
+            if (scPos != -1) {
+                actionName = actionName + actionMethod.substring(scPos);
+                actionMethod = actionMethod.substring(0, scPos);
+            }
+
+            mapping.setName(actionName);
             if (allowDynamicMethodCalls) {
-                mapping.setMethod(name.substring(exclamation + 1));
+                mapping.setMethod(actionMethod);
             } else {
                 mapping.setMethod(null);
             }

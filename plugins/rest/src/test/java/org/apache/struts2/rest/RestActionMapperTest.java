5eaef08e52fbb46e1659b46435172d8e52b8b090
Aleksandr Mashchenko <amashchenko@apache.org>
2016-01-14 20:01:57 +0200

    WW-4585 Struts2 Rest plugin doesn't handle JSESSIONID with DMI

--- plugins/rest/src/test/java/org/apache/struts2/rest/RestActionMapperTest.java
+++ plugins/rest/src/test/java/org/apache/struts2/rest/RestActionMapperTest.java
@@ -179,6 +179,35 @@ public class RestActionMapperTest extends TestCase {
         assertEquals("show", mapping.getMethod());
     }
 
+    public void testGetJsessionIdSemicolonMappingWithMethod() throws Exception {
+        req.setRequestURI("/myapp/animals/dog/fido!update;jsessionid=29fefpv23do1g");
+        req.setServletPath("/animals/dog/fido");
+        req.setMethod("GET");
+        
+        ActionMapping mapping = mapper.getMapping(req, configManager);
+
+        assertEquals("/animals", mapping.getNamespace());
+        assertEquals("dog", mapping.getName());
+        assertEquals("fido", ((String[]) mapping.getParams().get("id"))[0]);
+        assertEquals("show", mapping.getMethod());
+    }
+
+    public void testGetJsessionIdSemicolonMappingWithMethodAllowDMI() throws Exception {
+        req.setRequestURI("/myapp/animals/dog/fido!update;jsessionid=29fefpv23do1g");
+        req.setServletPath("/animals/dog/fido");
+        req.setMethod("GET");
+        
+        // allow DMI
+        mapper.setAllowDynamicMethodCalls("true");
+        
+        ActionMapping mapping = mapper.getMapping(req, configManager);
+
+        assertEquals("/animals", mapping.getNamespace());
+        assertEquals("dog", mapping.getName());
+        assertEquals("fido", ((String[]) mapping.getParams().get("id"))[0]);
+        assertEquals("update", mapping.getMethod());
+    }
+
     public void testParseNameAndNamespace() {
         tryUri("/foo/23", "", "foo/23");
         tryUri("/foo/", "", "foo/");

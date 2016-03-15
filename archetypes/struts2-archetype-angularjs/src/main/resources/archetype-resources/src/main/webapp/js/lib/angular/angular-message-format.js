d358f1f76dc3a64607ae3226ec025bda702c4cbb
Johannes Geppert <post@jgeppert.com>
2016-03-15 19:36:49 +0100

    Upgrade angular archetype to use latest AngularJS version 1.5
    https://issues.apache.org/jira/browse/WW-4522

--- archetypes/struts2-archetype-angularjs/src/main/resources/archetype-resources/src/main/webapp/js/lib/angular/angular-message-format.js
+++ archetypes/struts2-archetype-angularjs/src/main/resources/archetype-resources/src/main/webapp/js/lib/angular/angular-message-format.js
@@ -1,6 +1,6 @@
 /**
- * @license AngularJS v1.4.5
- * (c) 2010-2015 Google, Inc. http://angularjs.org
+ * @license AngularJS v1.5.0
+ * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
 (function(window, angular, undefined) {'use strict';

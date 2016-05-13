104bd92b88361978d3b8014db44e8a9a9b63077f
Lukasz Lenart <lukaszlenart@apache.org>
2016-05-13 08:00:04 +0200

    WW-4627 Adds unit test to confirm the issue

--- plugins/json/src/test/java/org/apache/struts2/json/JSONWriterTest.java
+++ plugins/json/src/test/java/org/apache/struts2/json/JSONWriterTest.java
@@ -7,7 +7,9 @@ import org.junit.Test;
 
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
@@ -88,6 +90,41 @@ public class JSONWriterTest extends StrutsTestCase{
         jsonWriter.setIgnoreHierarchy(false);
         String json = jsonWriter.write(bean1);
         TestUtils.assertEquals(JSONWriter.class.getResource("jsonwriter-write-bean-02.txt"), json);
+    }
+
+    @Test
+    public void testWriteBeanWithList() throws Exception {
+        BeanWithList bean1 = new BeanWithList();
+        bean1.setStringField("str");
+        bean1.setBooleanField(true);
+        bean1.setCharField('s');
+        bean1.setDoubleField(10.1);
+        bean1.setFloatField(1.5f);
+        bean1.setIntField(10);
+        bean1.setLongField(100);
+        bean1.setEnumField(AnEnum.ValueA);
+        bean1.setEnumBean(AnEnumBean.Two);
+        List<String> errors = new ArrayList<String>();
+        errors.add("Field is required");
+        bean1.setErrors(errors);
+
+        JSONWriter jsonWriter = new JSONWriter();
+        jsonWriter.setEnumAsBean(false);
+        jsonWriter.setIgnoreHierarchy(false);
+        String json = jsonWriter.write(bean1);
+        TestUtils.assertEquals(JSONWriter.class.getResource("jsonwriter-write-bean-04.txt"), json);
+    }
+
+    private class BeanWithList extends Bean {
+        private List<String> errors;
+
+        public List<String> getErrors() {
+            return errors;
+        }
+
+        public void setErrors(List<String> errors) {
+            this.errors = errors;
+        }
     }
 
     private class AnnotatedBean extends Bean{

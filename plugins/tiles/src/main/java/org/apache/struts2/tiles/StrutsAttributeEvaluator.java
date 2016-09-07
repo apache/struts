431e50e92242b24b98f36eaec9ac84c938bb82b6
Lukasz Lenart <lukaszlenart@apache.org>
2016-09-07 09:27:43 +0200

    WW-4685 Uses dedicated prefixes to access I18N and Struts internals

--- plugins/tiles/src/main/java/org/apache/struts2/tiles/StrutsAttributeEvaluator.java
+++ plugins/tiles/src/main/java/org/apache/struts2/tiles/StrutsAttributeEvaluator.java
@@ -20,9 +20,6 @@
 package org.apache.struts2.tiles;
 
 import com.opensymphony.xwork2.ActionContext;
-import com.opensymphony.xwork2.LocaleProvider;
-import com.opensymphony.xwork2.TextProvider;
-import com.opensymphony.xwork2.TextProviderFactory;
 import com.opensymphony.xwork2.config.ConfigurationException;
 import com.opensymphony.xwork2.ognl.OgnlUtil;
 import com.opensymphony.xwork2.util.logging.Logger;
@@ -43,8 +40,6 @@ public class StrutsAttributeEvaluator extends AbstractAttributeEvaluator {
 
     public Object evaluate(String expression, TilesRequestContext request) {
         try {
-            Object result = null;
-
             HttpServletRequest httpRequest = ServletUtil.getServletRequest(request).getRequest();
             ActionContext ctx = ServletActionContext.getActionContext(httpRequest);
 
@@ -53,34 +48,14 @@ public class StrutsAttributeEvaluator extends AbstractAttributeEvaluator {
                 throw new ConfigurationException("There is no ActionContext for current request!");
             }
 
-            TextProviderFactory tpf = new TextProviderFactory();
-            ctx.getContainer().inject(tpf);
-            LocaleProvider localeProvider = ctx.getContainer().getInstance(LocaleProvider.class);
+            OgnlUtil ognlUtil = ctx.getContainer().getInstance(OgnlUtil.class);
 
-            TextProvider textProvider = tpf.createInstance(ctx.getActionInvocation().getAction().getClass(), localeProvider);
-
-            if (textProvider != null) {
-                LOG.debug("Trying find text [{}] using TextProvider {}", expression, textProvider);
-                result = textProvider.getText(expression);
-                if (expression.equals(result)) {
-                    LOG.debug("Could not evaluate expression [{}] as a I18N key", expression);
-                    result = null;
-                }
-            }
-
-            if (result == null) {
-                OgnlUtil ognlUtil = ctx.getContainer().getInstance(OgnlUtil.class);
-                LOG.debug("Trying evaluate expression [{}] using OgnlUtil's getValue", expression);
-                result = ognlUtil.getValue(expression, ctx.getContextMap(), ctx.getValueStack().getRoot());
-            }
+            LOG.debug("Trying evaluate expression [{}] using OgnlUtil's getValue", expression);
+            Object result = ognlUtil.getValue(expression, ctx.getContextMap(), ctx.getValueStack().getRoot());
 
             LOG.debug("Final result of evaluating expression [{}] is: {}", expression, result);
 
-            if (result == null) {
-                return expression;
-            } else {
-                return result;
-            }
+            return result;
         } catch (OgnlException e) {
             throw new EvaluationException(e);
         }

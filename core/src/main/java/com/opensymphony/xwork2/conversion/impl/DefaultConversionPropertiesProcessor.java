98467c2753a81b45fe174492e3b779860c47a4b0
Lukasz Lenart <lukaszlenart@apache.org>
2017-03-23 15:27:55 +0100

    Adds missing headers with license

--- core/src/main/java/com/opensymphony/xwork2/conversion/impl/DefaultConversionPropertiesProcessor.java
+++ core/src/main/java/com/opensymphony/xwork2/conversion/impl/DefaultConversionPropertiesProcessor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2006,2009 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.opensymphony.xwork2.conversion.impl;
 
 import com.opensymphony.xwork2.XWorkException;
@@ -16,9 +31,6 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 
-/**
- * TODO lukaszlenart: add a comment
- */
 public class DefaultConversionPropertiesProcessor implements ConversionPropertiesProcessor {
 
     private static final Logger LOG = LogManager.getLogger(DefaultConversionPropertiesProcessor.class);
@@ -59,11 +71,11 @@ public class DefaultConversionPropertiesProcessor implements ConversionPropertie
                     String key = (String) entry.getKey();
 
                     try {
-                        TypeConverter _typeConverter = converterCreator.createTypeConverter((String) entry.getValue());
+                        TypeConverter typeConverter = converterCreator.createTypeConverter((String) entry.getValue());
                         if (LOG.isDebugEnabled()) {
-                            LOG.debug("\t{}:{} [treated as TypeConverter {}]", key, entry.getValue(), _typeConverter);
+                            LOG.debug("\t{}:{} [treated as TypeConverter {}]", key, entry.getValue(), typeConverter);
                         }
-                        converterHolder.addDefaultMapping(key, _typeConverter);
+                        converterHolder.addDefaultMapping(key, typeConverter);
                     } catch (Exception e) {
                         LOG.error("Conversion registration error", e);
                     }

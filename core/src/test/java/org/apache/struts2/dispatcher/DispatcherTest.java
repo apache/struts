bbbe2a80356811ff4dbaa99da2417a067eb614cc
Stefaan Dutry <stefaan.dutry@gmail.com>
2017-07-25 14:30:07 +0200

    WW-4818 added a couple of simple tests for MULTIPART_FORM_DATA_REGEX

--- core/src/test/java/org/apache/struts2/dispatcher/DispatcherTest.java
+++ core/src/test/java/org/apache/struts2/dispatcher/DispatcherTest.java
@@ -146,6 +146,32 @@ public class DispatcherTest extends StrutsInternalTestCase {
         assertTrue(wrapped instanceof MultiPartRequestWrapper);
     }
 
+    public void testPrepareMultipartRequestAllAllowedCharacters() throws Exception {
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        MockHttpServletResponse res = new MockHttpServletResponse();
+
+        req.setMethod("post");
+        req.setContentType("multipart/form-data; boundary=01=23a.bC:D((e)d'z?p+o_r,e-");
+        Dispatcher du = initDispatcher(Collections.<String, String>emptyMap());
+        du.prepare(req, res);
+        HttpServletRequest wrapped = du.wrapRequest(req);
+
+        assertTrue(wrapped instanceof MultiPartRequestWrapper);
+    }
+
+    public void testPrepareMultipartRequestIllegalCharacter() throws Exception {
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        MockHttpServletResponse res = new MockHttpServletResponse();
+
+        req.setMethod("post");
+        req.setContentType("multipart/form-data; boundary=01=2;3a.bC:D((e)d'z?p+o_r,e-");
+        Dispatcher du = initDispatcher(Collections.<String, String>emptyMap());
+        du.prepare(req, res);
+        HttpServletRequest wrapped = du.wrapRequest(req);
+
+        assertFalse(wrapped instanceof MultiPartRequestWrapper);
+    }
+
     public void testDispatcherListener() throws Exception {
     	
     	final DispatcherListenerState state = new DispatcherListenerState();

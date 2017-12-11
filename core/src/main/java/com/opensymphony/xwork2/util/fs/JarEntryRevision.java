94758b699cfea1bb8bfba777dcc383b4dcd41436
Yasser Zamani <yasserzamani@apache.org>
2017-12-12 00:26:44 +0330

    WW-4901 Decouples Struts from URL.openConnection implementation of container

--- core/src/main/java/com/opensymphony/xwork2/util/fs/JarEntryRevision.java
+++ core/src/main/java/com/opensymphony/xwork2/util/fs/JarEntryRevision.java
@@ -23,7 +23,6 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 import java.io.IOException;
-import java.net.JarURLConnection;
 import java.net.URL;
 
 /**
@@ -37,11 +36,9 @@ public class JarEntryRevision extends Revision {
     private long lastModified;
 
     public static Revision build(URL fileUrl, FileManager fileManager) {
-        // File within a Jar
-        // Find separator index of jar filename and filename within jar
-        JarURLConnection conn = null;
+        StrutsJarURLConnection conn = null;
         try {
-            conn = (JarURLConnection) fileUrl.openConnection();
+            conn = new StrutsJarURLConnection(fileUrl);
             conn.setUseCaches(false);
             URL url = fileManager.normalizeToFileProtocol(fileUrl);
             if (url != null) {
@@ -55,10 +52,7 @@ public class JarEntryRevision extends Revision {
         }
         finally {
             if(null != conn) {
-                try {
-                    conn.getInputStream().close();
-                } catch (IOException ignored) {
-                }
+                conn.disconnect();
             }
         }
     }
@@ -72,20 +66,17 @@ public class JarEntryRevision extends Revision {
     }
 
     public boolean needsReloading() {
-        JarURLConnection conn = null;
+        StrutsJarURLConnection conn = null;
         long lastLastModified = lastModified;
         try {
-            conn = (JarURLConnection) jarFileURL.openConnection();
+            conn = new StrutsJarURLConnection(jarFileURL);
             conn.setUseCaches(false);
             lastLastModified = conn.getJarEntry().getTime();
         } catch (IOException ignored) {
         }
         finally {
             if(null != conn) {
-                try {
-                    conn.getInputStream().close();
-                } catch (IOException ignored) {
-                }
+                conn.disconnect();
             }
         }
 

dd8ba56425d5cab99b4c03312c56a78e22749663
Lukasz Lenart <lukaszlenart@apache.org>
2018-01-01 15:45:15 +0100

    Extends factories to allow return implementing type

--- core/src/main/java/com/opensymphony/xwork2/inject/Factory.java
+++ core/src/main/java/com/opensymphony/xwork2/inject/Factory.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2006 Google Inc.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,12 +23,20 @@ package com.opensymphony.xwork2.inject;
  */
 public interface Factory<T> {
 
-  /**
-   * Creates an object to be injected.
-   *
-   * @param context of this injection
-   * @return instance to be injected
-   * @throws Exception if unable to create object
-   */
-  T create(Context context) throws Exception;
+    /**
+     * Creates an object to be injected.
+     *
+     * @param context of this injection
+     * @return instance to be injected
+     * @throws Exception if unable to create object
+     */
+    T create(Context context) throws Exception;
+
+    /**
+     * Returns a class of <T>
+     *
+     * @return class of the object
+     */
+    Class<? extends T> type();
+
 }

2b2de35519ede4627944f18feb349641b3be57ec
Hedju Hor <hedjuhor1@gmail.com>
2018-01-12 18:26:27 +0100

    Revert "fixed some Logger calls. Hint:https://logging.apache.org/log4j/2.0/manual/api.html"
    This reverts commit 87b2bd8

--- plugins/bean-validation/src/main/java/org/apache/struts/beanvalidation/validation/interceptor/BeanValidationInterceptor.java
+++ plugins/bean-validation/src/main/java/org/apache/struts/beanvalidation/validation/interceptor/BeanValidationInterceptor.java
@@ -94,11 +94,13 @@ public class BeanValidationInterceptor extends MethodFilterInterceptor {
         ActionProxy actionProxy = invocation.getProxy();
         String methodName = actionProxy.getMethod();
 
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Validating [{}/{}] with method [{}]", invocation.getProxy().getNamespace(), invocation.getProxy().getActionName(), methodName);
+        }
         Class<?>[] validationGroup = getValidationGroups(action, methodName);
 
         if (null == MethodUtils.getAnnotation(getActionMethod(action.getClass(), methodName), SkipValidation.class,
                 true, true)) {
-            LOG.debug("Validating [{}/{}] with method [{}] and groups [{}]", invocation.getProxy().getNamespace(), invocation.getProxy().getActionName(), methodName, Arrays.toString(validationGroup));
             // performing bean validation on action
             performBeanValidation(action, validator, validationGroup);
         }
@@ -113,7 +115,7 @@ public class BeanValidationInterceptor extends MethodFilterInterceptor {
 
     protected void performBeanValidation(Object action, Validator validator, Class<?>[] groups) {
 
-        LOG.trace("Initiating bean validation..");
+        LOG.trace("Initiating bean validation.. with groups [{}]", Arrays.toString(groups));
 
         Set<ConstraintViolation<Object>> constraintViolations;
 
@@ -154,7 +156,9 @@ public class BeanValidationInterceptor extends MethodFilterInterceptor {
                     if (action instanceof ModelDriven && fieldName.startsWith(ValidatorConstants.MODELDRIVEN_PREFIX)) {
                         fieldName = fieldName.replace("model.", ValidatorConstants.EMPTY_SPACE);
                     }
-                    LOG.debug("Adding field error [{}] with message [{}]", fieldName, validationError.getMessage());
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("Adding field error [{}] with message [{}]", fieldName, validationError.getMessage());
+                    }
                     validatorContext.addFieldError(fieldName, validationError.getMessage());
                 }
             }

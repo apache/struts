d5926b7a2196fab0735acac094b6438bb7cf4a94
Lukasz Lenart <lukaszlenart@apache.org>
2018-01-25 12:27:54 +0100

    Searches hierarchy down to Object class for annotation

--- core/src/main/java/com/opensymphony/xwork2/util/AnnotationUtils.java
+++ core/src/main/java/com/opensymphony/xwork2/util/AnnotationUtils.java
@@ -139,16 +139,13 @@ public class AnnotationUtils {
      */
     public static <T extends Annotation> T findAnnotation(Class<?> clazz, Class<T> annotationClass) {
         T ann = clazz.getAnnotation(annotationClass);
-        while (ann == null && clazz != null) {
+        if (ann == null && clazz != Object.class) {
             ann = clazz.getAnnotation(annotationClass);
             if (ann == null) {
                 ann = clazz.getPackage().getAnnotation(annotationClass);
             }
-            if (ann == null) {
-                clazz = clazz.getSuperclass();
-                if (clazz != null) {
-                    ann = clazz.getAnnotation(annotationClass);
-                }
+            if (ann == null && clazz != Object.class) {
+                ann = findAnnotation(clazz.getSuperclass(), annotationClass);
             }
         }
 

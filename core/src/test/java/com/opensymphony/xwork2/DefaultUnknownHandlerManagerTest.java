61fb20fc727b3a777b0032acd794036daa5c347d
Ziyad Alsaeed <zalsaeed@cs.uoregon.edu>
2018-01-25 23:20:03 -0800

    fixed the licensing and authore comments issues that causes build to fail

--- core/src/test/java/com/opensymphony/xwork2/DefaultUnknownHandlerManagerTest.java
+++ core/src/test/java/com/opensymphony/xwork2/DefaultUnknownHandlerManagerTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.opensymphony.xwork2;
 
 import com.opensymphony.xwork2.config.entities.ActionConfig;
@@ -8,7 +26,6 @@ import java.util.ArrayList;
 /**
  * Partial test to the DefaultUnknownHandlerManager to understand the relationship between Manager and Handlers.
  *
- * @author Ziyad Alsaeed
  */
 public class DefaultUnknownHandlerManagerTest extends XWorkTestCase {
 
@@ -19,7 +36,6 @@ public class DefaultUnknownHandlerManagerTest extends XWorkTestCase {
     /**
      * Relationshsip when UnknownAction method is called.
      *
-     * @author Ziyad Alsaeed
      */
     public void testHandleUnknownAction() {
 
@@ -35,7 +51,6 @@ public class DefaultUnknownHandlerManagerTest extends XWorkTestCase {
     /**
      * Relationship when UnknownActionMethod method called.
      *
-     * @author Ziyad Alsaeed
      */
     public void testHandelUnknownActionMethod() throws Exception {
         DefaultUnknownHandlerManager defaultUnknownHandlerManager = new DefaultUnknownHandlerManager();

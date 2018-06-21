a75eddb5154f9705bd2bac39f704512c8453e0b9
Yasser Zamani <yasserzamani@apache.org>
2018-06-21 12:00:46 +0430

    delete redundant code for performance

--- core/src/main/java/com/opensymphony/xwork2/ognl/SecurityMemberAccess.java
+++ core/src/main/java/com/opensymphony/xwork2/ognl/SecurityMemberAccess.java
@@ -146,8 +146,7 @@ public class SecurityMemberAccess extends DefaultMemberAccess {
         memberPackageName = memberPackageName + ".";
         
         for (String packageName: excludedPackageNames) {
-            if (targetPackageName.startsWith(packageName) || targetPackageName.equals(packageName)
-                    || memberPackageName.startsWith(packageName) || memberPackageName.equals(packageName)) {
+            if (targetPackageName.startsWith(packageName) || memberPackageName.startsWith(packageName)) {
                 return true;
             }
         }

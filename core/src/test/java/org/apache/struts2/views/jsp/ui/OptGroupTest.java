94210a02e73a32a0e20d111ac8270191ee2cb5d6
Aleksandr Mashchenko <amashchenko@apache.org>
2018-12-04 22:26:26 +0200

    WW-4987 Add test for listCssClass in optgroup tag

--- core/src/test/java/org/apache/struts2/views/jsp/ui/OptGroupTest.java
+++ core/src/test/java/org/apache/struts2/views/jsp/ui/OptGroupTest.java
@@ -253,4 +253,33 @@ public class OptGroupTest extends AbstractUITagTest {
         verify(SelectTag.class.getResource("OptGroup-6.txt"));
     }
 
+    public void testOptGroupListCssClass() throws Exception {
+        SelectTag selectTag = new SelectTag();
+        selectTag.setName("mySelection");
+        selectTag.setLabel("My Selection");
+        selectTag.setList("%{#{'ONE':'one','TWO':'two','THREE':'three'}}");
+        selectTag.setListCssClass("'option-css-class'");
+
+        OptGroupTag optGroupTag1 = new OptGroupTag();
+        optGroupTag1.setLabel("My Label 1");
+        optGroupTag1.setList("%{#{'AAA':'aaa','BBB':'bbb','CCC':'ccc'}}");
+        optGroupTag1.setListCssClass("'optgroup-option-css-class'");
+
+        OptGroupTag optGroupTag2 = new OptGroupTag();
+        optGroupTag2.setLabel("My Label 2");
+        optGroupTag2.setList("%{#{'DDD':'ddd','EEE':'eee','FFF':'fff'}}");
+
+        selectTag.setPageContext(pageContext);
+        selectTag.doStartTag();
+        optGroupTag1.setPageContext(pageContext);
+        optGroupTag1.doStartTag();
+        optGroupTag1.doEndTag();
+        optGroupTag2.setPageContext(pageContext);
+        optGroupTag2.doStartTag();
+        optGroupTag2.doEndTag();
+        selectTag.doEndTag();
+
+        //System.out.println(writer.toString());
+        verify(SelectTag.class.getResource("OptGroup-8.txt"));
+    }
 }

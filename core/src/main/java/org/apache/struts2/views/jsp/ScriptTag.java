9570aa707589751dc619c20fb6ba7091d7362caa
Ecenaz Jen Ozmen <eo2419@columbia.edu>
2020-07-15 10:34:14 +0000

    added script tag attributes

--- core/src/main/java/org/apache/struts2/views/jsp/ScriptTag.java
+++ core/src/main/java/org/apache/struts2/views/jsp/ScriptTag.java
@@ -9,8 +9,29 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ScriptTag extends ComponentTagSupport {
 
+    protected String async;
+    protected String charset;
+    protected String defer;
+    protected String src;
+    protected String type;
+
     @Override
     public Component getBean(ValueStack stack, HttpServletRequest req, HttpServletResponse res) {
         return new Script();
     }
+
+    protected void populateParams() {
+        super.populateParams();;
+        Script script = ((Script) component);
+        script.setAsync(async);
+        script.setCharset(charset);
+        script.setDefer(defer);
+        script.setSrc(src);
+        script.setType(type);
+    }
+
+    public void setAsync(String async) {
+        this.async = async;
+    }
+
 }

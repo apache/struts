63c955d135c7e1be5fde5d13375f43a03fce156f
Ecenaz Jen Ozmen <eo2419@columbia.edu>
2020-07-15 08:22:20 +0000

    script tag files

--- core/src/main/java/org/apache/struts2/views/jsp/ScriptTag.java
+++ core/src/main/java/org/apache/struts2/views/jsp/ScriptTag.java
@@ -1,5 +1,16 @@
 package org.apache.struts2.views.jsp;
 
-public class ScriptTag {
+import com.opensymphony.xwork2.util.ValueStack;
+import org.apache.struts2.components.Component;
+import org.apache.struts2.components.Script;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ScriptTag extends ComponentTagSupport {
+
+    @Override
+    public Component getBean(ValueStack stack, HttpServletRequest req, HttpServletResponse res) {
+        return new Script();
+    }
 }

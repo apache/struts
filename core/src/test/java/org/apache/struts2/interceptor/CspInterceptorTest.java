0e905e489495055cd5796749b9e603cb7e5e69d6
Giannis Chatziveroglou <giannisc@google.com>
2020-07-16 12:25:20 +0000

    csp interceptor tests updated

--- core/src/test/java/org/apache/struts2/interceptor/CspInterceptorTest.java
+++ core/src/test/java/org/apache/struts2/interceptor/CspInterceptorTest.java
@@ -8,6 +8,14 @@ import org.apache.struts2.StrutsInternalTestCase;
 import org.apache.struts2.interceptor.csp.CspInterceptor;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import static org.apache.struts2.interceptor.csp.CspSettings.CSP_HEADER;
+import static org.apache.struts2.interceptor.csp.CspSettings.SCRIPT_SRC;
+import static org.apache.struts2.interceptor.csp.CspSettings.STRICT_DYNAMIC;
+import static org.apache.struts2.interceptor.csp.CspSettings.OBJECT_SRC;
+import static org.apache.struts2.interceptor.csp.CspSettings.NONE;
+import static org.apache.struts2.interceptor.csp.CspSettings.BASE_URI;
+import static org.apache.struts2.interceptor.csp.CspSettings.HTTP;
+import static org.apache.struts2.interceptor.csp.CspSettings.HTTPS;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,15 +33,28 @@ public class CspInterceptorTest extends StrutsInternalTestCase {
         interceptor.intercept(mai);
         assertTrue("Nonce key does not exist", session.containsKey("nonce"));
         assertFalse("Nonce value is empty", Strings.isEmpty((String) session.get("nonce")));
+        checkHeader();
     }
 
     public void testNonceExists() throws Exception {
-        mai.getInvocationContext().getSession().put("nonce", "foo");
+        session.put("nonce", "foo");
         interceptor.intercept(mai);
         assertTrue("Nonce key does not exist", session.containsKey("nonce"));
         assertFalse("Nonce value is empty", Strings.isEmpty((String) session.get("nonce")));
+        checkHeader();
     }
-    
+
+    public void checkHeader(){
+        String header = String.format("%s '%s'; %s 'nonce-%s' '%s' %s %s; %s '%s';",
+                OBJECT_SRC, NONE,
+                SCRIPT_SRC, session.get("nonce"), STRICT_DYNAMIC, HTTP, HTTPS,
+                BASE_URI, NONE
+        );
+        String expectedCspHeader = response.getHeader(CSP_HEADER);
+        assertFalse("No CSP header exists", Strings.isEmpty(header));
+        assertEquals("Repsonse headers does not contain nonce header", expectedCspHeader, header);
+    }
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();

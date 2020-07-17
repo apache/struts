fbfb39e80d1f3db742810c1565c6748ba3e00294
Ecenaz Jen Ozmen <eo2419@columbia.edu>
2020-07-17 10:26:45 +0000

    added licences, decsription to scripttag, fixed compiler errors

--- core/src/main/java/org/apache/struts2/views/jsp/ui/ScriptTag.java
+++ core/src/main/java/org/apache/struts2/views/jsp/ui/ScriptTag.java
@@ -1,14 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.struts2.views.jsp.ui;
 
 import com.opensymphony.xwork2.util.ValueStack;
 import org.apache.struts2.components.Component;
 import org.apache.struts2.components.Script;
-import org.apache.struts2.views.jsp.ComponentTagSupport;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ScriptTag extends ComponentTagSupport {
+public class ScriptTag extends AbstractUITag {
 
     protected String async;
     protected String charset;

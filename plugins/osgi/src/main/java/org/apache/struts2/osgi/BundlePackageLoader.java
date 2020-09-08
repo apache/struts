10a834a54caac96d66c1fc91cb154a9e4369c780
JCgH4164838Gh792C124B5 <43964333+JCgH4164838Gh792C124B5@users.noreply.github.com>
2020-09-08 15:25:02 -0400

    Backport a few PR#433 OSGi plugin-related changes from 2.6 to 2.5. - Fix to allow the OSGi plugin to initialize with no internet connection. - Fix an unintended JS change for the admin bundle. - Fix the OSGi Demo bundle so it works under 2.5.x as well.

--- plugins/osgi/src/main/java/org/apache/struts2/osgi/BundlePackageLoader.java
+++ plugins/osgi/src/main/java/org/apache/struts2/osgi/BundlePackageLoader.java
@@ -24,11 +24,11 @@ import com.opensymphony.xwork2.config.Configuration;
 import com.opensymphony.xwork2.config.ConfigurationException;
 import com.opensymphony.xwork2.config.entities.PackageConfig;
 import com.opensymphony.xwork2.config.impl.DefaultConfiguration;
-import com.opensymphony.xwork2.config.providers.XmlConfigurationProvider;
 import com.opensymphony.xwork2.inject.Container;
 import com.opensymphony.xwork2.util.fs.DefaultFileManagerFactory;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.struts2.config.StrutsXmlConfigurationProvider;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
@@ -140,18 +140,18 @@ public class BundlePackageLoader implements PackageLoader {
         this.contextContainer = contextContainer;
     }
 
-    static class BundleConfigurationProvider extends XmlConfigurationProvider {
+    static class BundleConfigurationProvider extends StrutsXmlConfigurationProvider {
         private Bundle bundle;
         private BundleContext bundleContext;
 
         public BundleConfigurationProvider(String filename, Bundle bundle, BundleContext bundleContext) {
-            super(filename, false);
+            super(filename, false, null);  // Currently no dependency on ServletContext
             this.bundle = bundle;
             this.bundleContext = bundleContext;
         }
 
         public BundleConfigurationProvider(String filename) {
-            super(filename);
+            super(filename, false, null);  // Currently no dependency on ServletContext
         }
 
         @Override

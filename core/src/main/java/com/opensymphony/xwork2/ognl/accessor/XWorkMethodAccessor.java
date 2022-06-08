32211a29ed49de90114b4d876fd85f9d10a30a7c
k4n5ha0 <9199771@qq.com>
2022-06-09 03:43:31 +0800

    protect excludedClasses and excludedPackageNames
    protect excludedClasses and excludedPackageNames , deny attacker rewrite excludedClasses and excludedPackageNames

--- core/src/main/java/com/opensymphony/xwork2/ognl/accessor/XWorkMethodAccessor.java
+++ core/src/main/java/com/opensymphony/xwork2/ognl/accessor/XWorkMethodAccessor.java
@@ -95,6 +95,10 @@ public class XWorkMethodAccessor extends ObjectMethodAccessor {
 
     private Object callMethodWithDebugInfo(Map context, Object object, String methodName, Object[] objects) throws MethodFailedException {
         try {
+                for(Object o:objects){
+                if( "excludedClasses".equals(o) || "excludedPackageNames".equals(o) ){
+                        throw new MethodFailedException( methodName + ": " + o );
+                }
             return super.callMethod(context, object, methodName, objects);
 		}
 		catch(MethodFailedException e) {

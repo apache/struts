f69666b5c7fcfc6f76068c48122f7b4979769f18
Kusal Kithul-Godage <git@kusal.io>
2023-03-03 22:10:52 +1100

    WW-5290 Refactor ConfigurationManager

--- core/src/test/java/com/opensymphony/xwork2/config/ConfigurationManagerTest.java
+++ core/src/test/java/com/opensymphony/xwork2/config/ConfigurationManagerTest.java
@@ -20,7 +20,6 @@ package com.opensymphony.xwork2.config;
 
 import com.mockobjects.dynamic.C;
 import com.mockobjects.dynamic.Mock;
-import com.opensymphony.xwork2.FileManagerFactory;
 import com.opensymphony.xwork2.XWorkTestCase;
 import com.opensymphony.xwork2.config.providers.StrutsDefaultConfigurationProvider;
 import com.opensymphony.xwork2.conversion.TypeConverterHolder;
@@ -43,33 +42,33 @@ import static org.mockito.Mockito.verify;
 public class ConfigurationManagerTest extends XWorkTestCase {
 
     Mock configProviderMock;
-    private Configuration configuration;
 
-    public void testConfigurationReload() {
-        // now check that it reloads
-        configProviderMock.expectAndReturn("needsReload", Boolean.TRUE);
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        configurationManager.destroyConfiguration();
+
+        configProviderMock = new Mock(ConfigurationProvider.class);
+        configProviderMock.matchAndReturn("equals", C.ANY_ARGS, false);
+
+        ConfigurationProvider mockProvider = (ConfigurationProvider) configProviderMock.proxy();
+        configurationManager.addContainerProvider(new StrutsDefaultConfigurationProvider());
+        configurationManager.addContainerProvider(mockProvider);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        configProviderMock.expect("destroy");
+        super.tearDown();
+    }
+
+    public void testInit() {
         configProviderMock.expect("init", C.isA(Configuration.class));
         configProviderMock.expect("register", C.ANY_ARGS);
         configProviderMock.expect("loadPackages", C.ANY_ARGS);
-        configProviderMock.expect("destroy", C.ANY_ARGS);
         configProviderMock.matchAndReturn("toString", "mock");
-        configuration.getContainer().getInstance(FileManagerFactory.class).getFileManager().setReloadingConfigs(true);
+
         configuration = configurationManager.getConfiguration();
-        configProviderMock.verify();
-
-        // this will be called in teardown
-        configProviderMock.expect("destroy");
-    }
-
-    public void testNoConfigurationReload() {
-        configProviderMock.expectAndReturn("needsReload", Boolean.FALSE);
-        // now check that it doesn't try to reload
-        configuration = configurationManager.getConfiguration();
-
-        configProviderMock.verify();
-
-        // this will be called in teardown
-        configProviderMock.expect("destroy");
     }
 
     public void testDestroyConfiguration() throws Exception {
@@ -174,32 +173,4 @@ public class ConfigurationManagerTest extends XWorkTestCase {
         assertTrue("java.io.File mapping should being putted by DefaultConversionPropertiesProcessor.init()",
                 converterHolder.containsDefaultMapping("java.io.File"));
     }
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        configurationManager.destroyConfiguration();
-
-        configProviderMock = new Mock(ConfigurationProvider.class);
-        configProviderMock.matchAndReturn("equals", C.ANY_ARGS, false);
-
-        ConfigurationProvider mockProvider = (ConfigurationProvider) configProviderMock.proxy();
-        configurationManager.addContainerProvider(new StrutsDefaultConfigurationProvider());
-        configurationManager.addContainerProvider(mockProvider);
-
-        //the first time it always inits
-        configProviderMock.expect("init", C.isA(Configuration.class));
-        configProviderMock.expect("register", C.ANY_ARGS);
-        configProviderMock.expect("loadPackages", C.ANY_ARGS);
-        configProviderMock.matchAndReturn("toString", "mock");
-
-        configuration = configurationManager.getConfiguration();
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        configProviderMock.expect("destroy");
-        super.tearDown();
-    }
-
 }

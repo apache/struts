576c0646958de04891c0eca6db562478d57de337
Lukasz Lenart <lukaszlenart@apache.org>
2023-12-16 16:48:51 +0100

    Replaces empty file upload test with simple one

--- apps/showcase/src/test/java/it/org/apache/struts2/showcase/FileUploadTest.java
+++ apps/showcase/src/test/java/it/org/apache/struts2/showcase/FileUploadTest.java
@@ -19,35 +19,46 @@
 package it.org.apache.struts2.showcase;
 
 import java.io.File;
+import java.io.FileWriter;
 
 import org.htmlunit.WebClient;
-import org.htmlunit.html.DomElement;
 import org.htmlunit.html.HtmlFileInput;
 import org.htmlunit.html.HtmlForm;
 import org.htmlunit.html.HtmlInput;
 import org.htmlunit.html.HtmlPage;
 import org.htmlunit.html.HtmlSubmitInput;
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FileUploadTest {
 
     @Test
-    public void testEmptyFile() throws Exception {
+    public void testSimpleFileUpload() throws Exception {
         try (final WebClient webClient = new WebClient()) {
             final HtmlPage page = webClient.getPage(ParameterUtils.getBaseUrl() + "/fileupload/doUpload.action");
             final HtmlForm form = page.getFormByName("doUpload");
             HtmlInput captionInput = form.getInputByName("caption");
             HtmlFileInput uploadInput = form.getInputByName("upload");
             captionInput.type("some caption");
-            File tempFile = File.createTempFile("testEmptyFile", ".tmp");
+            File tempFile = File.createTempFile("testEmptyFile", ".txt");
             tempFile.deleteOnExit();
+
+            try (FileWriter writer = new FileWriter(tempFile)) {
+                writer.append("Some strings");
+            }
+
             uploadInput.setValue(tempFile.getAbsolutePath());
             final HtmlSubmitInput button = form.getInputByValue("Submit");
             final HtmlPage resultPage = button.click();
-            DomElement errorMessage = resultPage.getFirstByXPath("//span[@class='errorMessage']");
-            Assert.assertNotNull(errorMessage);
-            Assert.assertEquals("File cannot be empty", errorMessage.getVisibleText());
+
+            String content = resultPage.getVisibleText();
+            System.out.println(content);
+            assertThat(content).contains(
+                    "ContentType: text/plain",
+                    "Original FileName: " + tempFile.getName(),
+                    "Caption:some caption"
+            );
         }
     }
 

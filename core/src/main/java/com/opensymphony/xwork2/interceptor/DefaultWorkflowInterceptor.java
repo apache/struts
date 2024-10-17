ebedd7391fec19a039d00190d97bbd5705f2f45d
Kusal Kithul-Godage <git@kusal.io>
2024-10-17 19:28:41 +1100

    WW-3714 Marker interface migration follow-up

--- core/src/main/java/com/opensymphony/xwork2/interceptor/DefaultWorkflowInterceptor.java
+++ core/src/main/java/com/opensymphony/xwork2/interceptor/DefaultWorkflowInterceptor.java
@@ -25,6 +25,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.MethodUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.struts2.interceptor.ValidationAware;
+import org.apache.struts2.interceptor.ValidationErrorAware;
+import org.apache.struts2.interceptor.ValidationWorkflowAware;
 
 /**
  * <!-- START SNIPPET: description -->

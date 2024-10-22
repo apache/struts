2757c23572f56335219fdad5cad285e79dcbbcfa
Kusal Kithul-Godage <git@kusal.io>
2024-10-22 16:48:49 +1100

    WW-3714 Fix replacement ValidationAware marker not recognised

--- core/src/main/java/com/opensymphony/xwork2/interceptor/ValidationAware.java
+++ core/src/main/java/com/opensymphony/xwork2/interceptor/ValidationAware.java
@@ -18,9 +18,89 @@
  */
 package com.opensymphony.xwork2.interceptor;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @deprecated since 6.7.0, use {@link org.apache.struts2.interceptor.ValidationAware} instead.
  */
 @Deprecated
 public interface ValidationAware extends org.apache.struts2.interceptor.ValidationAware {
+
+    static ValidationAware adapt(org.apache.struts2.interceptor.ValidationAware actualValidation) {
+        if (actualValidation instanceof ValidationAware) {
+            return (ValidationAware) actualValidation;
+        }
+        return actualValidation != null ? new LegacyAdapter(actualValidation) : null;
+    }
+
+    class LegacyAdapter implements ValidationAware {
+
+        private final org.apache.struts2.interceptor.ValidationAware adaptee;
+
+        private LegacyAdapter(org.apache.struts2.interceptor.ValidationAware adaptee) {
+            this.adaptee = adaptee;
+        }
+
+        @Override
+        public void setActionErrors(Collection<String> errorMessages) {
+            adaptee.setActionErrors(errorMessages);
+        }
+
+        @Override
+        public Collection<String> getActionErrors() {
+            return adaptee.getActionErrors();
+        }
+
+        @Override
+        public void setActionMessages(Collection<String> messages) {
+            adaptee.setActionMessages(messages);
+        }
+
+        @Override
+        public Collection<String> getActionMessages() {
+            return adaptee.getActionMessages();
+        }
+
+        @Override
+        public void setFieldErrors(Map<String, List<String>> errorMap) {
+            adaptee.setFieldErrors(errorMap);
+        }
+
+        @Override
+        public Map<String, List<String>> getFieldErrors() {
+            return adaptee.getFieldErrors();
+        }
+
+        @Override
+        public void addActionError(String anErrorMessage) {
+            adaptee.addActionError(anErrorMessage);
+        }
+
+        @Override
+        public void addActionMessage(String aMessage) {
+            adaptee.addActionMessage(aMessage);
+        }
+
+        @Override
+        public void addFieldError(String fieldName, String errorMessage) {
+            adaptee.addFieldError(fieldName, errorMessage);
+        }
+
+        @Override
+        public boolean hasActionErrors() {
+            return adaptee.hasActionErrors();
+        }
+
+        @Override
+        public boolean hasActionMessages() {
+            return adaptee.hasActionMessages();
+        }
+
+        @Override
+        public boolean hasFieldErrors() {
+            return adaptee.hasFieldErrors();
+        }
+    }
 }

2a1051fe39f6d7bf24ee29a5173c2d3a81529cc1
Lukasz Lenart <lukaszlenart@apache.org>
2024-11-10 12:11:46 +0100

    WW-5455 Defines a new plugin to support Jasper Reports 7

---
+++ plugins/jasperreports7/src/main/java/org/apache/struts2/views/jasperreports7/JasperReportConstants.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.struts2.views.jasperreports7;
+
+
+/**
+ * <code>JasperReportConstants</code>
+ */
+public interface JasperReportConstants {
+
+    /**
+     * PDF format constant
+     */
+    String FORMAT_PDF = "PDF";
+
+    /**
+     * XML format constant
+     */
+    String FORMAT_XML = "XML";
+
+    /**
+     * HTML format constant
+     */
+    String FORMAT_HTML = "HTML";
+
+    /**
+     * XLS format constant
+     */
+    String FORMAT_XLSX = "XLSX";
+
+    /**
+     * CSV format constant
+     */
+    String FORMAT_CSV = "CSV";
+
+    /**
+     * RTF format constant
+     */
+    String FORMAT_RTF = "RTF";
+}
